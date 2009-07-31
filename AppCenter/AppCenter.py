1dedaf8c55fcdaf6c713fcc85554ba862a3a6e5b
Michael Vogt <michael.vogt@ubuntu.com>
2009-07-31 22:13:44 +0200

    add navigation buttons

--- AppCenter/AppCenter.py
+++ AppCenter/AppCenter.py
@@ -39,13 +39,14 @@ class AppCenter(SimpleGtkbuilderApp):
         self.view_switcher = ViewSwitcher()
         self.scrolledwindow_viewswitcher.add(self.view_switcher)
         self.view_switcher.show()
-        self.view_switcher.connect("row-activated", self.view_switcher_activated)
+        self.view_switcher.connect("row-activated", 
+                                   self.on_view_switcher_activated)
 
         # categories
         self.cat_view = CategoriesView(APP_INSTALL_PATH, self.xapiandb, self.icons)
         self.scrolledwindow_categories.add(self.cat_view)
         self.cat_view.show()
-        self.cat_view.connect("item-activated", self.category_activated)
+        self.cat_view.connect("item-activated", self.on_category_activated)
         
         # apps
         empty_store = gtk.ListStore(gtk.gdk.Pixbuf, str)
@@ -68,14 +69,29 @@ class AppCenter(SimpleGtkbuilderApp):
         self.apps_sorted = True
         self.apps_limit = 0
 
+    # xapian query
     def get_query_from_search_entry(self, search_term):
         """ get xapian.Query from a search term string """
         parser = xapian.QueryParser()
         query = parser.parse_query(search_term)
         return query
 
+    # navigation buttons
+    def add_navigation_button(self, name, callback):
+        button = gtk.Button()
+        button.set_label(name)
+        button.show()
+        button.connect("clicked", callback)
+        self.hbox_navigation_buttons.pack_start(button, expand=False)
+
+    def remove_navigation_buttons(self):
+        for w in self.hbox_navigation_buttons:
+            self.hbox_navigation_buttons.remove(w)
+
+    # callbacks
     def on_button_home_clicked(self, widget):
         logging.debug("on_button_home_clicked")
+        self.apps_category_query = None
         self.notebook_view.set_current_page(self.NOTEBOOK_PAGE_CATEGORIES)
 
     def on_entry_search_changed(self, widget):
@@ -95,6 +111,40 @@ class AppCenter(SimpleGtkbuilderApp):
     def on_button_search_entry_clear_clicked(self, widget):
         self.entry_search.set_text("")
 
+    def on_view_switcher_activated(self, view_switcher, row, column):
+        logging.debug("view_switcher_activated")
+        model = view_switcher.get_model()
+        action = model[row][ViewSwitcherList.COL_ACTION]
+        if action == ViewSwitcherList.ACTION_ITEM_AVAILABLE:
+            logging.debug("show available")
+            self.apps_filter = None
+            self.refresh_apps()
+        elif action == ViewSwitcherList.ACTION_ITEM_INSTALLED:
+            logging.debug("show installed")
+            self.apps_filter = self.installed_filter.filter
+            self.refresh_apps()
+        elif action == ViewSwitcherList.ACTION_ITEM_PENDING:
+            logging.debug("show pending")
+        else:
+            assert False, "Not reached"
+
+    def on_navigation_button_category(self, widget):
+        self.notebook_view.set_current_page(self.NOTEBOOK_PAGE_APPLIST)
+
+    def on_navigation_button_app_details(self, widget):
+        self.notebook_view.set_current_page(self.NOTEBOOK_PAGE_APP_DETAILS)
+
+    def on_category_activated(self, cat_view, path):
+        (name, pixbuf, query) = cat_view.get_model()[path]
+        self.apps_category_query = query
+        # show new category
+        self.refresh_apps()
+        self.notebook_view.set_current_page(self.NOTEBOOK_PAGE_APPLIST)
+        # update navigation bar
+        self.remove_navigation_buttons()
+        self.add_navigation_button(name, self.on_navigation_button_category)
+
+    # gui helper
     def refresh_apps(self):
         # build query
         if self.apps_category_query and self.apps_search_query:
@@ -118,28 +168,6 @@ class AppCenter(SimpleGtkbuilderApp):
         id = self.statusbar_main.get_context_id("items")
         self.statusbar_main.push(id, _("%s items available") % len(new_model))
 
-    def view_switcher_activated(self, view_switcher, row, column):
-        logging.debug("view_switcher_activated")
-        model = view_switcher.get_model()
-        action = model[row][ViewSwitcherList.COL_ACTION]
-        if action == ViewSwitcherList.ACTION_ITEM_AVAILABLE:
-            logging.debug("show available")
-            self.apps_filter = None
-            self.refresh_apps()
-        elif action == ViewSwitcherList.ACTION_ITEM_INSTALLED:
-            logging.debug("show installed")
-            self.apps_filter = self.installed_filter.filter
-            self.refresh_apps()
-        elif action == ViewSwitcherList.ACTION_ITEM_PENDING:
-            logging.debug("show pending")
-        else:
-            assert False, "Not reached"
-
-    def category_activated(self, cat_view, path):
-        (name, pixbuf, query) = cat_view.get_model()[path]
-        self.apps_category_query = query
-        self.refresh_apps()
-        self.notebook_view.set_current_page(self.NOTEBOOK_PAGE_APPLIST)
 
     def run(self):
         self.window_main.show_all()
