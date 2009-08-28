c67aa80315cfaada9be240d1a67eeb7e263cb178
Michael Vogt <michael.vogt@ubuntu.com>
2009-08-28 09:43:14 +0200

    softwarestore/view/animatedicon.py: add animated icon based on the code from rugby471

---
+++ softwarestore/view/animatedicon.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2009 Canonical
+#
+# Authors:
+#  rugby471
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import gobject
+import gtk
+import os
+import glob
+
+
+class AnimatedImage(gtk.Image):
+    
+    FPS = 25.0
+
+    def __init__(self, globexp):
+        super(AnimatedImage, self).__init__()
+        self._progressN = 0
+        self.images = sorted(glob.glob(globexp))
+        self.set_from_file(self.images[self._progressN])
+        source_id = gobject.timeout_add(1000/self.FPS, self.progressIconTimeout)
+
+    def progressIconTimeout(self):
+        self._progressN += 1
+        if self._progressN == len(self.images):
+            self._progressN = 0
+        image.set_from_file(self.images[self._progressN])
+        return True
+
+if __name__ == "__main__":
+
+    if os.path.exists("./data"):
+        datadir = "./data/"
+    else:
+        datadir = "/usr/share/software-store/"
+
+    image = AnimatedImage(datadir+"/icons/32x32/status/softwarestore_progress_*.png")
+
+    win = gtk.Window()
+    win.add(image)
+    win.set_size_request(400,400)
+    win.show_all()
+
+    gtk.main()
+
+
