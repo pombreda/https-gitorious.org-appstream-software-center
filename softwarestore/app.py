e168cbfa7b985c83fe75bb52a3f75fdf42ebeffd
Michael Vogt <michael.vogt@ubuntu.com>
2009-09-09 10:05:22 +0200

    softwarestore/app.py: doc update

--- softwarestore/app.py
+++ softwarestore/app.py
@@ -214,12 +214,13 @@ class SoftwareStoreApp(SimpleGtkbuilderApp):
              "/usr/bin/software-properties-gtk", 
              "-n", 
              "-t", str(self.window_main.window.xid)])
-        # return code of 1 means that it changed
+        # wait for it to finish
         ret = None
         while ret is None:
             while gtk.events_pending():
                 gtk.main_iteration()
             ret = p.poll()
+        # return code of 1 means that it changed
         if ret == 1:
             self.run_update_cache()
         self.window_main.set_sensitive(True)
