d8f3278ebb9c76258f0cc1b264cae9c8315862e3
Andrew <rugby471@jaunty-toaster>
2009-09-20 17:36:42 +0100

    more history stuff

--- softwarestore/view/history.py
+++ softwarestore/view/history.py
@@ -20,13 +20,15 @@
 import gobject
 import gtk
 import os
-import time
+import datetime
 import logging
 import pango
 
 from random import *
 import xml.etree.ElementTree as ET
 
+ICON_SIZE = 24
+MISSING_APP_ICON = "/usr/share/icons/gnome/scalable/categories/applications-other.svg"
 HISTORY_FILE = "/tmp/history.xml"
 
 class PkgHistory():
@@ -48,12 +50,14 @@ class PkgHistory():
     
         self.history_root = tree.getroot()
     
-    def add_event(self):
+    def add_event(self, type, package_name):
         events = self.history_root.find("events")
         new_event = ET.SubElement(events, "event")
-        event_id = str(uniform(1, 100))
+        event_id = str(round(uniform(1, 100000000)))[:-2]
         new_event.set("id", event_id)
-        new_event.set("time", str(time.time()))
+        new_event.set("time", str(datetime.date.today()))
+        new_event.set("type", type)
+        new_event.set("package_name", package_name)
         return event_id
         
     def add_action(self, event_id, type, package_name):
@@ -64,6 +68,10 @@ class PkgHistory():
         new_action = ET.SubElement(event, "action")
         new_action.set("type", type)
         new_action.set("package_name", package_name)
+        
+    def list_events(self):
+        events = self.history_root.find("events")
+        return events.getchildren()
         
     def write(self):
         ET.ElementTree(self.history_root).write(HISTORY_FILE)
@@ -87,20 +95,27 @@ class HistoryView(gtk.TreeView):
         gtk.TreeView.__init__(self)
         self.set_fixed_height_mode(True)
         self.set_headers_visible(False)
+        
         tp = gtk.CellRendererPixbuf()
-        column = gtk.TreeViewColumn("Icon", tp)
+        column = gtk.TreeViewColumn("Icon", tp, pixbuf=0)
         column.set_fixed_width(32)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
         self.append_column(column)
         tr = gtk.CellRendererText()
         tr.set_property("ellipsize", pango.ELLIPSIZE_MIDDLE)
-        column = gtk.TreeViewColumn("Name", tr)
+        column = gtk.TreeViewColumn("Name", tr, markup=1)
         column.set_fixed_width(200)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
         self.append_column(column)
-        if store is None:
-            store = gtk.ListStore(str, gtk.gdk.Pixbuf)
-        self.set_model(store)
+        self.store = gtk.ListStore(gtk.gdk.Pixbuf, str)
+        self.set_model(self.store)
+        
+        events = history.list_events()
+        for event in events:
+            s = event.get("package_name")
+            pix = gtk.gdk.pixbuf_new_from_file_at_size(MISSING_APP_ICON, ICON_SIZE, ICON_SIZE)
+            self.store.append([pix, s])
+        
         # custom cursor
         self._cursor_hand = gtk.gdk.Cursor(gtk.gdk.HAND2)
         # our own "activate" handler
@@ -109,6 +124,7 @@ class HistoryView(gtk.TreeView):
         self.connect("button-press-event", self._on_button_press_event)
         self.connect("motion-notify-event", self._on_motion_notify_event)
         self.connect("cursor-changed", self._on_cursor_changed)
+        
     def _on_row_activated(self, treeview, path, column):
         (name, text, icon, overlay, pkgname) = treeview.get_model()[path]
         self.emit("application-activated", name, pkgname)
@@ -121,7 +137,7 @@ class HistoryView(gtk.TreeView):
         self.emit("application-selected", name, pkgname)
     def _on_motion_notify_event(self, widget, event):
         (rel_x, rel_y, width, height, depth) = widget.window.get_geometry()
-        if width - event.x <= AppStore.ICON_SIZE:
+        if width - event.x <= ICON_SIZE:
             self.window.set_cursor(self._cursor_hand)
         else:
             self.window.set_cursor(None)
@@ -141,18 +157,21 @@ class HistoryView(gtk.TreeView):
         # get the size of gdk window
         (rel_x, rel_y, width, height, depth) = widget.window.get_geometry()
         # the last pixels of the view are reserved for the arrow icon
-        if width - event.x <= AppStore.ICON_SIZE:
+        if width - event.x <= ICON_SIZE:
             self.emit("row-activated", path, column)
+
 
 
 if __name__ == "__main__":
     history = PkgHistory()
     history.open()
-    event_id = history.add_event()
+    event_id = history.add_event("install", "gnome-games")
     history.add_action(event_id, "install", "gnome-games")
     history.add_action(event_id, "install", "gnome-chess")
     history.write()
-    
+
+    view = HistoryView()
+
     # gui
     scroll = gtk.ScrolledWindow()
     view = HistoryView()
@@ -165,7 +184,8 @@ if __name__ == "__main__":
     box.pack_start(scroll)
 
     win = gtk.Window()
-    #scroll.add(view)
+    
+    scroll.add(view)
     win.add(box)
     win.set_size_request(400,400)
     win.show_all()
