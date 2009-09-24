0407e2eae3a6bb2cb6c578284f86b147945a89da
Michael Vogt <michael.vogt@ubuntu.com>
2009-09-24 09:03:22 +0200

    * softwarestore/view/pendingview.py:   - fix crash in cancel handling (LP: #435454)

--- softwarestore/view/pendingview.py
+++ softwarestore/view/pendingview.py
@@ -17,7 +17,7 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-
+import dbus
 import logging
 import gtk
 import gobject
