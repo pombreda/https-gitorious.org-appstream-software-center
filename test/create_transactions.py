a0aac83381510e5336eb9ac8a470d18526d3a1d9
Michael Vogt <michael.vogt@ubuntu.com>
2009-10-02 11:37:47 +0200

    test/create_transactions.py: make the transactions tester accept a limit

--- test/create_transactions.py
+++ test/create_transactions.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python
 
+import glib
+import logging
 import time
+
 from aptdaemon.client import AptClient
 
-c = AptClient()
-for i in range(100):
-    print i
-    t = c.commit_packages(["3dchess"], [], [], [], [], exit_handler=lambda x, y: True)
-    t.run(block=False)
-    t = c.commit_packages([], [], ["3dchess"], [], [], exit_handler=lambda x, y: True)
-    t.run(block=False)
+MAX_ACTIVE=1
+active = 0
 
-    time.sleep(5)
+def exit_handler(trans, enum):
+    global active
+    active -= 1
+    return True
 
+if __name__ == "__main__":
+    debconf_log = logging.getLogger("AptClient.DebconfProxy")
+    logging.basicConfig(level=logging.DEBUG)
+    print debconf_log
+    debconf_log.setLevel(logging.DEBUG)
+
+    context = glib.main_context_default()
+    c = AptClient()
+    for i in range(100):
+        logging.debug("loop: nr: %s active: %s" % (i, active))
+        
+        t = c.install_packages(["3dchess"], exit_handler=exit_handler)
+        t.run(block=False)
+        active += 1
+        t = c.install_packages(["2vcard"], exit_handler=exit_handler)
+        t.run(block=False)
+        active += 1
+        
+        t = c.remove_packages(["3dchess","2vcard"], exit_handler=exit_handler)
+        t.run(block=False)
+        active += 1
+
+        while active > MAX_ACTIVE:
+            while context.pending():
+                context.iteration()
+            time.sleep(1)
