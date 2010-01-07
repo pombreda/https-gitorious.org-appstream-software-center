0eb88effedf97489b1e90909086b09540fb46728
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-07 09:54:29 +0100

    softwarecenter/db/reviews.py: add review loader stub and Ipsum loader

---
+++ softwarecenter/db/reviews.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 Canonical
+#
+# Authors:
+#  Michael Vogt
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+import random
+import time
+
+class Review(object):
+    """A review object """
+    def __init__(self, app):
+        # a softwarecenter.db.database.Application object
+        self.app = app
+        # the review items that the object fills in
+        self.id = None
+        self.text = None
+        self.date = None
+        self.rating = None
+        self.person = None
+    def __repr__(self):
+        return "[Review id=%s text='%s' person='%s']" % (self.id, self.text, self.person)
+
+
+class ReviewLoader(object):
+    def get_reviews(self, application):
+        """returns a list of review objects for the given
+           db.database.Application object
+        """
+        return []
+
+
+class ReviewLoaderIpsum(ReviewLoader):
+    """ a test review loader that does not do any network io
+        and returns random lorem ipsum review texts
+    """
+    #This text is under public domain
+    #Lorem ipsum
+    #Cicero
+    LOREM=u"""lorem ipsum dolor öäü sit amet consetetur sadipscing elitr sed diam nonumy
+eirmod tempor invidunt ut labore et dolore magna aliquyam erat sed diam
+voluptua at vero eos et accusam et justo duo dolores et ea rebum stet clita
+kasd gubergren no sea takimata sanctus est lorem ipsum dolor sit amet lorem
+ipsum dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod
+tempor invidunt ut labore et dolore magna aliquyam erat sed diam voluptua at
+vero eos et accusam et justo duo dolores et ea rebum stet clita kasd
+gubergren no sea takimata sanctus est lorem ipsum dolor sit amet lorem ipsum
+dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor
+invidunt ut labore et dolore magna aliquyam erat sed diam voluptua at vero
+eos et accusam et justo duo dolores et ea rebum stet clita kasd gubergren no
+sea takimata sanctus est lorem ipsum dolor sit amet
+
+duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+molestie consequat vel illum dolore eu feugiat nulla facilisis at vero eros
+et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+delenit augue duis dolore te feugait nulla facilisi lorem ipsum dolor sit
+amet consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut
+laoreet dolore magna aliquam erat volutpat
+
+ut wisi enim ad minim veniam quis nostrud exerci tation ullamcorper suscipit
+lobortis nisl ut aliquip ex ea commodo consequat duis autem vel eum iriure
+dolor in hendrerit in vulputate velit esse molestie consequat vel illum
+dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
+dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te
+feugait nulla facilisi
+
+nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet
+doming id quod mazim placerat facer possim assum lorem ipsum dolor sit amet
+consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut
+laoreet dolore magna aliquam erat volutpat ut wisi enim ad minim veniam quis
+nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+commodo consequat
+
+duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+molestie consequat vel illum dolore eu feugiat nulla facilisis
+
+at vero eos et accusam et justo duo dolores et ea rebum stet clita kasd
+gubergren no sea takimata sanctus est lorem ipsum dolor sit amet lorem ipsum
+dolor sit amet consetetur sadipscing elitr sed diam nonumy eirmod tempor
+invidunt ut labore et dolore magna aliquyam erat sed diam voluptua at vero
+eos et accusam et justo duo dolores et ea rebum stet clita kasd gubergren no
+sea takimata sanctus est lorem ipsum dolor sit amet lorem ipsum dolor sit
+amet consetetur sadipscing elitr at accusam aliquyam diam diam dolore
+dolores duo eirmod eos erat et nonumy sed tempor et et invidunt justo labore
+stet clita ea et gubergren kasd magna no rebum sanctus sea sed takimata ut
+vero voluptua est lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore
+et dolore magna aliquyam erat
+
+consetetur sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore
+et dolore magna aliquyam erat sed diam voluptua at vero eos et accusam et
+justo duo dolores et ea rebum stet clita kasd gubergren no sea takimata
+sanctus est lorem ipsum dolor sit amet lorem ipsum dolor sit amet consetetur
+sadipscing elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat sed diam voluptua at vero eos et accusam et justo duo
+dolores et ea rebum stet clita kasd gubergren no sea takimata sanctus est
+lorem ipsum dolor sit amet lorem ipsum dolor sit amet consetetur sadipscing
+elitr sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+aliquyam erat sed diam voluptua at vero eos et accusam et justo duo dolores
+et ea rebum stet clita kasd gubergren no sea takimata sanctus est lorem
+ipsum dolor sit amet"""
+    USERS = ["joe", "john", "cat", "foo", "bar", "baz"]
+    def _random_person(self):
+        return random.choice(self.USERS)
+    def _random_text(self):
+        return random.choice(self.LOREM.split("\n\n"))
+    def get_reviews(self, application):
+        reviews = []
+        for i in range(0,random.randint(1,10)):
+            review = Review(application)
+            review.id = random.randint(1,5)
+            review.date = time.ctime(time.time())
+            review.person = self._random_person()
+            review.text = self._random_text()
+            reviews.append(review)
+        return reviews
+
+if __name__ == "__main__":
+    from softwarecenter.db.database import Application
+    app = Application("7zip",None)
+    loader = ReviewLoaderIpsum()
+    print loader.get_reviews(app)
