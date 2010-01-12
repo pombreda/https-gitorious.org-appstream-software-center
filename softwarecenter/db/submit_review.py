f33a2d59226a27492a75f4456b77300447ffb02d
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-12 11:16:48 +0100

    * softwarecenter/db/submit_review.py:   - code cleanup, nicer and still async now

--- softwarecenter/db/submit_review.py
+++ softwarecenter/db/submit_review.py
@@ -48,37 +48,76 @@ LOGIN_STATE_SUCCESS = "success"
 LOGIN_STATE_AUTH_FAILURE = "auth-fail"
 LOGIN_STATE_USER_CANCEL = "user-cancel"
 
-# the current login state, this is used accross multiple threads
-CURRENT_LOGIN_STATE = LOGIN_STATE_UNKNOWN
+class LaunchpadlibWorker(threading.Thread):
 
-# the entered username/pw
-LOGIN_USERNAME = None
-LOGIN_PASSWORD = None
+    def __init__(self):
+        # init parent
+        threading.Thread.__init__(self)
+        # the current login state, this is used accross multiple threads
+        self.login_state = LOGIN_STATE_UNKNOWN
+        # the username/pw to use
+        self.login_username = ""
+        self.login_password = ""
+        self._launchpad = None
 
+    def run(self):
+        print "lp worker thread run"
+        # login
+        self.lp_login()
+        # loop
+        self._wait_for_commands()
+
+    def _wait_for_commands(self):
+        while True:
+            print "worker: _wait_state_change"
+            time.sleep(0.2)
+
+    def lp_login(self):
+        print "lp_login"
+        # use cachedir
+        cachedir = os.path.expanduser("~/.cache/software-center")
+        if not os.path.exists(cachedir):
+            os.makedirs(cachedir)
+        print "hello"
+
+        # login into LP with GUI
+        self.launchpad = Launchpad.login_with(
+            'software-center', 'edge', cachedir,
+            allow_access_levels = ['WRITE_PUBLIC'],
+            authorizer_class=AuthorizeRequestTokenFromThread)
+        # if we are here we are in
+        self.login_state = LOGIN_STATE_SUCCESS
+        print "/done"
+        print self.launchpad
 
 class AuthorizeRequestTokenFromThread(RequestTokenAuthorizationEngine):
 
-    def input_username(self, cached_username, suggested_message):
-        """Collect the Launchpad username from the end-user.
+    def __new__(cls, *args, **kwargs):
+        o = object.__new__(cls)
+        # keep the state here (the lp_worker_thead is a singelton)
+        o.lp_worker = lp_worker_thread
+        return o
 
-        :param cached_username: A username from a previous entry attempt,
-        to be presented as the default.
-        """
-        global CURRENT_LOGIN_STATE 
+    def input_username(self, cached_username, suggested_message):
+        print "foo: ", self.lp_worker.login_state
         print "thread ask username"
-        # FIXME: check for valid state transitions
-        if CURRENT_LOGIN_STATE != LOGIN_STATE_ASK_USER_AND_PASS:
-            CURRENT_LOGIN_STATE = LOGIN_STATE_ASK_USER_AND_PASS
+        # check if user canceled and if so just return ""
+        if self.lp_worker.login_state == LOGIN_STATE_USER_CANCEL:
+            return ""
+        # otherwise go into ASK state
+        if self.lp_worker.login_state != LOGIN_STATE_ASK_USER_AND_PASS:
+            self.lp_worker.login_state = LOGIN_STATE_ASK_USER_AND_PASS
         # wait for username to become available
-        while not CURRENT_LOGIN_STATE in (LOGIN_STATE_HAS_USER_AND_PASS,
-                                          LOGIN_STATE_USER_CANCEL):
+        while not self.lp_worker.login_state in (LOGIN_STATE_HAS_USER_AND_PASS,
+                                                 LOGIN_STATE_USER_CANCEL):
             time.sleep(0.2)
-        return LOGIN_USERNAME
+        # note: returning None here make lplib open a registration page
+        #       in the browser
+        return self.lp_worker.login_username
 
     def input_password(self, suggested_message):
-        """Collect the Launchpad password from the end-user."""
         print "Input password"
-        return LOGIN_PASSWORD
+        return self.lp_worker.login_password
 
     def input_access_level(self, available_levels, suggested_message,
                            only_one_option=None):
@@ -92,43 +131,15 @@ class AuthorizeRequestTokenFromThread(RequestTokenAuthorizationEngine):
     def authentication_failure(self, suggested_message):
         """The user entered invalid credentials."""
         print "auth failure"
-        global CURRENT_LOGIN_STATE 
-        CURRENT_LOGIN_STATE = LOGIN_STATE_AUTH_FAILURE
+        if self.lp_worker.login_state == LOGIN_STATE_USER_CANCEL:
+            return
+        self.lp_worker.login_state = LOGIN_STATE_AUTH_FAILURE
 
     def success(self, suggested_message):
         """The token was successfully authorized."""
-        global CURRENT_LOGIN_STATE 
         print "success"
-        self.output(suggested_message)
-        CURRENT_LOGIN_STATE = LOGIN_STATE_SUCCESS
+        self.lp_worker.login_state = LOGIN_STATE_SUCCESS
 
-    # none of the below is needed
-    def output(self, message):
-        print self.text_wrapper.fill(message)
-
-    def user_refused_to_authorize(self, suggested_message):
-        """The user refused to authorize a request token."""
-        self.output(suggested_message)
-        self.output("\n")
-
-    def user_authorized(self, access_level, suggested_message):
-        """The user authorized a request token with some access level."""
-        self.output(suggested_message)
-        self.output("\n")
-
-    def server_consumer_differs_from_client_consumer(
-        self, client_name, real_name, suggested_message):
-        """The client seems to be lying or mistaken about its name.
-
-        When requesting a request token, the client told Launchpad
-        that its consumer name was "foo". Now the client is telling the
-        end-user that its name is "bar". Something is fishy and at the very
-        least the end-user should be warned about this.
-        """
-        self.output("\n")
-        self.output(suggested_message)
-        self.output("\n")
-        
 class SubmitReviewsApp(SimpleGtkbuilderApp):
     """ review a given application or package """
     def __init__(self, app, datadir):
@@ -139,20 +150,18 @@ class SubmitReviewsApp(SimpleGtkbuilderApp):
         gettext.textdomain("software-center")
         # data
         self.app = app
-        # launchpadlib worker thread
-        self._lp = LaunchpadlibWorker()
 
     def enter_username_password(self):
-        global CURRENT_LOGIN_STATE, LOGIN_USERNAME, LOGIN_PASSWORD
         res = self.dialog_review_login.run()
         self.dialog_review_login.hide()
         if res == gtk.RESPONSE_OK:
-            LOGIN_USERNAME = self.entry_review_login_email.get_text()
-            LOGIN_PASSWORD = self.entry_review_login_password.get_text()
-            CURRENT_LOGIN_STATE = LOGIN_STATE_HAS_USER_AND_PASS
+            username = self.entry_review_login_email.get_text()
+            lp_worker_thread.login_username = username
+            password = self.entry_review_login_password.get_text()
+            lp_worker_thread.login_password = password
+            lp_worker_thread.login_state = LOGIN_STATE_HAS_USER_AND_PASS
         else:
-            CURRENT_LOGIN_STATE = LOGIN_STATE_USER_CANCEL
-            return ""
+            lp_worker_thread.login_state = LOGIN_STATE_USER_CANCEL
 
     def enter_review(self):
         res = self.dialog_review_app.run()
@@ -171,54 +180,23 @@ class SubmitReviewsApp(SimpleGtkbuilderApp):
 
     def run(self):
         # do the launchpad stuff async
-        self._lp.start()
-        # wait for global state change 
-        glib.timeout_add(2000, self._wait_state_change)
+        lp_worker_thread.start()
+        # wait for  state change 
+        glib.timeout_add(200, self._wait_for_login)
         # parent
         SimpleGtkbuilderApp.run(self)
     
-    def _wait_state_change(self):
-        print "gui: _wait_state_change: ", CURRENT_LOGIN_STATE
-        if CURRENT_LOGIN_STATE == LOGIN_STATE_ASK_USER_AND_PASS:
+    def _wait_for_login(self):
+        print "gui: _wait_state_change: ", lp_worker_thread.login_state
+        if lp_worker_thread.login_state == LOGIN_STATE_ASK_USER_AND_PASS:
             self.enter_username_password()
-        elif CURRENT_LOGIN_STATE == LOGIN_STATE_SUCCESS:
+        elif lp_worker_thread.login_state == LOGIN_STATE_SUCCESS:
             self.enter_review()
+            return False
         return True
 
-class LaunchpadlibWorker(threading.Thread):
-
-    def run(self):
-        print "lp worker thread run"
-        self.launchpad = None
-        self.lp_login()
-
-    def _wait_state_change(self):
-        while True:
-            print "worker: _wait_state_change"
-            time.sleep(2)
-
-    def lp_login(self):
-        global CURRENT_LOGIN_STATE 
-        print "lp_login"
-        # use cachedir
-        cachedir = os.path.expanduser("~/.cache/software-center")
-        if not os.path.exists(cachedir):
-            os.makedirs(cachedir)
-        print "hello"
-
-        # login into LP with GUI
-        self.launchpad = Launchpad.login_with(
-            'software-center', 'edge', cachedir,
-            allow_access_levels = ['WRITE_PUBLIC'],
-            authorizer_class=AuthorizeRequestTokenFromThread)
-        # if we are here we are in
-        CURRENT_LOGIN_STATE = LOGIN_STATE_SUCCESS
-        print CURRENT_LOGIN_STATE
-        print "/done"
-        print self.launchpad
-
-        self._wait_state_change()
-        
+# IMPORTANT: create one (module) global LP worker thread here
+lp_worker_thread = LaunchpadlibWorker()
 
 if __name__ == "__main__":
     # FIXME: provide a package
