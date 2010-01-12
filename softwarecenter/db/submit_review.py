569f7bb94b5b46b5b60aaabc28e5fad0c14123cf
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-12 10:36:28 +0100

    * softwarecenter/db/submit_review.py:   - full async version of the login process, ugly because of the globals

--- softwarecenter/db/submit_review.py
+++ softwarecenter/db/submit_review.py
@@ -18,9 +18,18 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import pygtk
+pygtk.require ("2.0")
+import gobject
+gobject.threads_init()
+
+import gettext
+import glib
+import gtk
 import os
 import sys
-import gtk
+import time
+import threading
 
 from gettext import gettext as _
 from launchpadlib.launchpad import Launchpad
@@ -29,9 +38,25 @@ from launchpadlib import uris
 from urlparse import urljoin
 
 import softwarecenter.view.dialogs
+from softwarecenter.SimpleGtkbuilderApp import SimpleGtkbuilderApp
 
-# FIXME: make this (much) more async 
-class AuthorizeRequestTokenWithGUI(RequestTokenAuthorizationEngine):
+# the various states that the login can be in
+LOGIN_STATE_UNKNOWN = "unkown"
+LOGIN_STATE_ASK_USER_AND_PASS = "ask-user-and-pass"
+LOGIN_STATE_HAS_USER_AND_PASS = "has-user-pass"
+LOGIN_STATE_SUCCESS = "success"
+LOGIN_STATE_AUTH_FAILURE = "auth-fail"
+LOGIN_STATE_USER_CANCEL = "user-cancel"
+
+# the current login state, this is used accross multiple threads
+CURRENT_LOGIN_STATE = LOGIN_STATE_UNKNOWN
+
+# the entered username/pw
+LOGIN_USERNAME = None
+LOGIN_PASSWORD = None
+
+
+class AuthorizeRequestTokenFromThread(RequestTokenAuthorizationEngine):
 
     def input_username(self, cached_username, suggested_message):
         """Collect the Launchpad username from the end-user.
@@ -39,56 +64,44 @@ class AuthorizeRequestTokenWithGUI(RequestTokenAuthorizationEngine):
         :param cached_username: A username from a previous entry attempt,
         to be presented as the default.
         """
-        print "input username"
-        d = self.gtk_builder.get_object("dialog_review_login")
-        res = d.run()
-        d.hide()
-        if res == gtk.RESPONSE_OK:
-            entry = self.gtk_builder.get_object("entry_review_login_email")
-            return entry.get_text()
-        else:
-            self.user_canceled = True
-            return ""
+        global CURRENT_LOGIN_STATE 
+        print "thread ask username"
+        # FIXME: check for valid state transitions
+        if CURRENT_LOGIN_STATE != LOGIN_STATE_ASK_USER_AND_PASS:
+            CURRENT_LOGIN_STATE = LOGIN_STATE_ASK_USER_AND_PASS
+        # wait for username to become available
+        while not CURRENT_LOGIN_STATE in (LOGIN_STATE_HAS_USER_AND_PASS,
+                                          LOGIN_STATE_USER_CANCEL):
+            time.sleep(0.2)
+        return LOGIN_USERNAME
 
     def input_password(self, suggested_message):
         """Collect the Launchpad password from the end-user."""
         print "Input password"
-        # we rely on that input_username was run before and presented
-        # the dialog already
-        entry = self.gtk_builder.get_object("entry_review_login_password")
-        return entry.get_text()
+        return LOGIN_PASSWORD
 
     def input_access_level(self, available_levels, suggested_message,
                            only_one_option=None):
         """Collect the desired level of access from the end-user."""
+        print "input_access_level"
         return "WRITE_PUBLIC"
 
-    def _gtk_builder_init(self, datadir="./data", domain="software-center"):
-        self.gtk_builder = gtk.Builder()
-        self.gtk_builder.set_translation_domain(domain)
-        self.gtk_builder.add_from_file(datadir+"/ui/reviews.ui")
-        self.gtk_builder.connect_signals(self)
-        for o in self.gtk_builder.get_objects():
-            if issubclass(type(o), gtk.Buildable):
-                name = gtk.Buildable.get_name(o)
-                setattr(self, name, o)
-
     def startup(self, suggested_messages):
-        # construct gtkbuilder stuff on statup
-        self.user_canceled = False
-        self._gtk_builder_init()
+        print "startup"
 
     def authentication_failure(self, suggested_message):
         """The user entered invalid credentials."""
-        # do not show anything if the user canceled
-        if self.user_canceled:
-            return
-        # show error
-        softwarecenter.view.dialogs.error(self.dialog_review_login,
-                                          _("Authentication failure"),
-                                          _("I can not login with the "
-                                            "the given credentials. "
-                                            "Please try again."))
+        print "auth failure"
+        global CURRENT_LOGIN_STATE 
+        CURRENT_LOGIN_STATE = LOGIN_STATE_AUTH_FAILURE
+
+    def success(self, suggested_message):
+        """The token was successfully authorized."""
+        global CURRENT_LOGIN_STATE 
+        print "success"
+        self.output(suggested_message)
+        CURRENT_LOGIN_STATE = LOGIN_STATE_SUCCESS
+
     # none of the below is needed
     def output(self, message):
         print self.text_wrapper.fill(message)
@@ -115,26 +128,33 @@ class AuthorizeRequestTokenWithGUI(RequestTokenAuthorizationEngine):
         self.output("\n")
         self.output(suggested_message)
         self.output("\n")
-
-    def success(self, suggested_message):
-        """The token was successfully authorized."""
-        self.output(suggested_message)
         
-class ReviewApplicationOrPackage(object):
+class SubmitReviewsApp(SimpleGtkbuilderApp):
     """ review a given application or package """
-    def __init__(self, launchpad, app, 
-                 datadir="./data", domain="software-center"):
-        self._launchpad = launchpad
-        self.gtk_builder = gtk.Builder()
-        self.gtk_builder.set_translation_domain(domain)
-        self.gtk_builder.add_from_file(datadir+"/ui/reviews.ui")
-        self.gtk_builder.connect_signals(self)
-        for o in self.gtk_builder.get_objects():
-            if issubclass(type(o), gtk.Buildable):
-                name = gtk.Buildable.get_name(o)
-                setattr(self, name, o)
+    def __init__(self, app, datadir):
+        SimpleGtkbuilderApp.__init__(self, 
+                                     datadir+"/ui/reviews.ui",
+                                     "software-center")
+        gettext.bindtextdomain("software-center", "/usr/share/locale")
+        gettext.textdomain("software-center")
+        # data
+        self.app = app
+        # launchpadlib worker thread
+        self._lp = LaunchpadlibWorker()
 
-    def run(self):
+    def enter_username_password(self):
+        global CURRENT_LOGIN_STATE, LOGIN_USERNAME, LOGIN_PASSWORD
+        res = self.dialog_review_login.run()
+        self.dialog_review_login.hide()
+        if res == gtk.RESPONSE_OK:
+            LOGIN_USERNAME = self.entry_review_login_email.get_text()
+            LOGIN_PASSWORD = self.entry_review_login_password.get_text()
+            CURRENT_LOGIN_STATE = LOGIN_STATE_HAS_USER_AND_PASS
+        else:
+            CURRENT_LOGIN_STATE = LOGIN_STATE_USER_CANCEL
+            return ""
+
+    def enter_review(self):
         res = self.dialog_review_app.run()
         print res
         if res == gtk.RESPONSE_OK:
@@ -149,21 +169,59 @@ class ReviewApplicationOrPackage(object):
             test_bug.description = text
             test_bug.lp_save()
 
+    def run(self):
+        # do the launchpad stuff async
+        self._lp.start()
+        # wait for global state change 
+        glib.timeout_add(2000, self._wait_state_change)
+        # parent
+        SimpleGtkbuilderApp.run(self)
+    
+    def _wait_state_change(self):
+        print "gui: _wait_state_change: ", CURRENT_LOGIN_STATE
+        if CURRENT_LOGIN_STATE == LOGIN_STATE_ASK_USER_AND_PASS:
+            self.enter_username_password()
+        elif CURRENT_LOGIN_STATE == LOGIN_STATE_SUCCESS:
+            self.enter_review()
+        return True
+
+class LaunchpadlibWorker(threading.Thread):
+
+    def run(self):
+        print "lp worker thread run"
+        self.launchpad = None
+        self.lp_login()
+
+    def _wait_state_change(self):
+        while True:
+            print "worker: _wait_state_change"
+            time.sleep(2)
+
+    def lp_login(self):
+        global CURRENT_LOGIN_STATE 
+        print "lp_login"
+        # use cachedir
+        cachedir = os.path.expanduser("~/.cache/software-center")
+        if not os.path.exists(cachedir):
+            os.makedirs(cachedir)
+        print "hello"
+
+        # login into LP with GUI
+        self.launchpad = Launchpad.login_with(
+            'software-center', 'edge', cachedir,
+            allow_access_levels = ['WRITE_PUBLIC'],
+            authorizer_class=AuthorizeRequestTokenFromThread)
+        # if we are here we are in
+        CURRENT_LOGIN_STATE = LOGIN_STATE_SUCCESS
+        print CURRENT_LOGIN_STATE
+        print "/done"
+        print self.launchpad
+
+        self._wait_state_change()
+        
 
 if __name__ == "__main__":
-
-    # use cachedir
-    cachedir = os.path.expanduser("~/.cache/software-center")
-    if not os.path.exists(cachedir):
-        os.makedirs(cachedir)
-        
-    # login into LP with GUI
-    launchpad = Launchpad.login_with(
-        'software-center', 'edge', cachedir,
-        allow_access_levels = ['WRITE_PUBLIC'],
-        authorizer_class=AuthorizeRequestTokenWithGUI)
-    
     # FIXME: provide a package
-    review_app = ReviewApplicationOrPackage(launchpad, None)
+    review_app = SubmitReviewsApp(datadir="./data", app="2vcard")
     review_app.run()
 
