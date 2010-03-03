b62110c7d27bac0eeeecf476ffc67a47d045cf2d
Michael Vogt <michael.vogt@ubuntu.com>
2010-03-03 22:56:46 +0100

    * softwarecenter/view/navhistory.py:   - fix missing import

--- softwarecenter/view/navhistory.py
+++ softwarecenter/view/navhistory.py
@@ -16,6 +16,7 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 import copy
+import logging
 
 # FIXME: sucks, move elsewhere
 in_replay_history_mode = False
