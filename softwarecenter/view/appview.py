0ba415729008516b33367a02d6c3260a8d5030b5
Julian Andres Klode <jak@debian.org>
2010-03-29 15:07:21 +0200

    * softwarecenter/view/appview.py:   - Add from __future__ import with_statement for Python 2.5

--- softwarecenter/view/appview.py
+++ softwarecenter/view/appview.py
@@ -16,6 +16,8 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+from __future__ import with_statement
+
 
 import apt
 import gettext
