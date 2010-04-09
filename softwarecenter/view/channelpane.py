71b76dc54c271e5791ac4b34e5cb522dad27b9ca
Michael Vogt <michael.vogt@ubuntu.com>
2010-04-09 23:09:38 +0200

    softwarecenter/view/channelpane.py: set busy cursor

--- softwarecenter/view/channelpane.py
+++ softwarecenter/view/channelpane.py
@@ -106,6 +106,7 @@ class ChannelPane(SoftwarePane):
 
     def _make_new_model(self, query, seq_nr):
         # get a new store and attach it to the view
+        self.scroll_app_list.window.set_cursor(self.busy_cursor)
         new_model = AppStore(self.cache,
                              self.db, 
                              self.icons, 
@@ -115,11 +116,12 @@ class ChannelPane(SoftwarePane):
                              filter=self.apps_filter)
         # between request of the new model and actual delivery other
         # events may have happend
+        self.scroll_app_list.window.set_cursor(None)
         if seq_nr == self.refresh_seq_nr:
             self.app_view.set_model(new_model)
+            self.emit("app-list-changed", len(new_model))
         else:
             logging.debug("discarding new model (%s != %s)" % (seq_nr, self.refresh_seq_nr))
-        self.emit("app-list-changed", len(new_model))
         return False
 
     def set_channel(self, channel):
@@ -180,7 +182,11 @@ class ChannelPane(SoftwarePane):
         if self.notebook.get_current_page() == self.PAGE_APP_DETAILS:
             return ""
         # otherwise, show status based on search or not
-        length = len(self.app_view.get_model())
+        model = self.app_view.get_model()
+        if model:
+            length = len(self.app_view.get_model())
+        else:
+            length = 0
         if len(self.searchentry.get_text()) > 0:
             return gettext.ngettext("%s matching item",
                                     "%s matching items",
