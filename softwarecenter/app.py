73a92835621e5b124974022598fd9184cefc7a18
Kiwinote <kiwinote@gmail.com>
2010-06-30 10:07:53 +0100

    Viewswitcher shouldn't load listview for installed_pane if we are navigating to a details view

--- softwarecenter/app.py
+++ softwarecenter/app.py
@@ -360,7 +360,7 @@ class SoftwareCenterApp(SimpleGtkbuilderApp):
         else:
             self.menuitem_go_back.set_sensitive(False)
             self.menuitem_go_forward.set_sensitive(False)
-        if action == self.NOTEBOOK_PAGE_INSTALLED and not self.installed_pane.loaded:
+        if action == self.NOTEBOOK_PAGE_INSTALLED and not self.installed_pane.loaded and not self.installed_pane.current_appview_selection:
             self.installed_pane.refresh_apps()
         # switch to new page
         self.notebook_view.set_current_page(action)
