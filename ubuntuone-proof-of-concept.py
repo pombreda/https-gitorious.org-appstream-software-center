645be048c3fa9277587b9c588b65e5f126b038f9
and471 rugby471@gmail.com <>
2010-07-14 17:10:29 +0100

    removing all login dialog & ubuntuone stuff - This is just the dialog work now.
    Changes:
    	Made dialogs comply with spec
    	Made icon size bigger and increased padding for appview

--- ubuntuone-proof-of-concept.py
+++
@@ -1,8 +0,0 @@
-import gtk
-
-builder = gtk.Builder()
-builder.add_from_file("Unsaved 2.ui")
-
-builder.get_object("window1").show_all()
-builder.get_object("window1").connect("delete-event", gtk.main_quit)
-gtk.main()
