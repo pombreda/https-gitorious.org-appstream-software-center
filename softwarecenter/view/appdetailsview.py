3205a8094d809628a52723ca4870dab68ebdfda0
Michael Vogt <michael.vogt@ubuntu.com>
2010-07-15 09:53:24 +0200

    make re-install purchased work

--- softwarecenter/view/appdetailsview.py
+++ softwarecenter/view/appdetailsview.py
@@ -106,6 +106,14 @@ class AppDetailsViewBase(object):
     def reinstall_purchased(self):
         """ reinstall a purchased app """
         print "reinstall_purchased",  self.app
+        appdetails = self.app.get_details(self.db)
+        deb_line = appdetails.deb_line
+        signing_key_id = appdetails.signing_key_id
+        get_install_backend().add_repo_add_key_and_install_app(deb_line,
+                                                               signing_key_id,
+                                                               self.app)
+
+        
 
     def get_icon_filename(self, iconname, iconsize):
         iconinfo = self.icons.lookup_icon(iconname, iconsize, 0)
