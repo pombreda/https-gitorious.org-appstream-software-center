b66fb021a172629a6e1bfdf57207f2da74ab43ea
Kiwinote <kiwinote@gmail.com>
2010-08-05 15:06:10 +0200

    fix crash on filtering by support status in purchases pane

--- softwarecenter/app.py
+++ softwarecenter/app.py
@@ -612,7 +612,7 @@ class SoftwareCenterApp(SimpleGtkbuilderApp):
         glib.timeout_add_seconds(1, lambda p: p.poll() == None, p)
 
     def on_menuitem_view_all_activate(self, widget):
-        if (not self._block_menuitem_view and
+        if (not self._block_menuitem_view and self.active_pane.apps_filter and
             self.active_pane.apps_filter.get_supported_only()):
             self.active_pane.apps_filter.set_supported_only(False)
             self.active_pane.refresh_apps()
