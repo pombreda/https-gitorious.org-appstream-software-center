b2e69c3315e5da1b29922caaed8ee315836e2159
Michael Vogt <michael.vogt@ubuntu.com>
2010-08-13 11:32:57 +0200

    softwarecenter/view/purchasedialog.py: add SOFTWARE_CENTER_DEBUG_BUY mode that fills the form in automatically (mostly)

--- softwarecenter/view/purchasedialog.py
+++ softwarecenter/view/purchasedialog.py
@@ -16,9 +16,12 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import glib
 import gtk
 import logging
+import os
 import simplejson
+import urllib
 import webkit
 
 from gettext import gettext as _
@@ -76,6 +79,9 @@ class PurchaseDialog(gtk.Dialog):
             self.wk.webkit.load_html_string(DUMMY_HTML, "file:///")
         self.vbox.pack_start(self.wk)
         self.distro = get_distro()
+        # only for debugging
+        if os.environ.get("SOFTWARE_CENTER_DEBUG_BUY"):
+            glib.timeout_add_seconds(1, _generate_events, self)
 
     def _on_create_webview_request(self, view, frame):
         logging.debug("_on_create_webview_request")
@@ -105,7 +111,7 @@ class PurchaseDialog(gtk.Dialog):
             res = simplejson.loads(json_string)
             #print res
         except:
-            logging.exception("error processing json")
+            logging.warn("error processing json: '%s'" % json_string)
             return
         if res["successful"] == False:
             self.hide()
@@ -134,7 +140,7 @@ class PurchaseDialog(gtk.Dialog):
                                                                signing_key_id,
                                                                self.app)
 
-# just used for testing
+# just used for testing --------------------------------------------
 DUMMY_HTML = """
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
        "http://www.w3.org/TR/html4/loose.dtd">
@@ -171,12 +177,67 @@ DUMMY_HTML = """
 </html>
     """
 
+# synthetic key event generation
+def _send_keys(dialog, s):
+    print "_send_keys", s
+    MAPPING = { '@'     : 'at',
+                '.'     : 'period',
+                '\t'    : 'Tab',
+                '\n'    : 'Return',
+                '?'     : 'question',
+                '\a'    : 'Down',  # fake 
+                ' '     : 'space',
+                '\v'    : 'Page_Down', # fake
+              }
+    
+    for key in s:
+        event = gtk.gdk.Event(gtk.gdk.KEY_PRESS)
+        event.window = dialog.window
+        if key.isdigit():
+            key = "_"+key
+        if hasattr(gtk.keysyms, key):
+            event.keyval = getattr(gtk.keysyms, key)
+        else:
+            event.keyval = getattr(gtk.keysyms, MAPPING[key])
+        gtk.main_do_event(event)
+  
+
+# \a means down key - its a just a fake to get it working
+LOGIN = os.environ.get("SOFTWARE_CENTER_LOGIN") or "michael.vogt@ubuntu.com"
+# for some reason the "space" key before on checkbox does not work when
+# the event is generated, so this needs to be done manually :/
+PAYMENT_DETAILS = "\tstreet1\tstreet2\tcity\tstate\t1234\t\a\t\a\a\t"\
+                  "ACCEPTED\t4111111111111111\t1234\t\a\t\a\a\t\t\t \v"
+# state-name, window title, keys
+STATES = [ ('login', 'Log in', LOGIN+"\t"),
+           ('confirm-sso', 'Authenticate to', '\n'),
+           ('enter-payment', 'Confirm Payment Details', PAYMENT_DETAILS),
+           ('confirm-payment', 'title-the-same-as-before', '\t\n'),
+           ('end-state', 'no-title', ''),
+         ]
+def _generate_events(dialog):
+    global STATES
+
+    (state, title, keys) = STATES[0]
+
+    print "_generate_events: in state", state
+
+    current_title = dialog.wk.webkit.get_property("title")
+    if current_title and current_title.startswith(title):
+        print "found state", state
+        _send_keys(dialog, keys)
+        STATES.pop(0)
+
+    return True
+
+def _on_key_press(dialog, event):
+    print event, event.keyval
+
 if __name__ == "__main__":
     #url = "http://www.animiertegifs.de/java-scripts/alertbox.php"
     #url = "http://www.ubuntu.com"
     #d = PurchaseDialog(app=None, html=DUMMY_HTML)
     #d = PurchaseDialog(app=None, url="http://spiegel.de")
-    import urllib
     from softwarecenter.enums import BUY_SOMETHING_HOST
     url = BUY_SOMETHING_HOST+"/subscriptions/en/ubuntu/maverick/+new/?%s" % ( 
         urllib.urlencode({
@@ -184,4 +245,8 @@ if __name__ == "__main__":
                 'arch' : "i386",
                 }))
     d = PurchaseDialog(app=None, url=url)
+    # useful for debugging
+    d.connect("key-press-event", _on_key_press)
+    #glib.timeout_add_seconds(1, _generate_events, d)
     d.run()
+    
