92866fbf116e0858a66bbbdbfaddc7322cdc1c86
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2010-09-07 22:11:39 +1200

    create class StatusBar from which PackageStatusBar and AddonsStatusBar inherit from.  Allows AddonsStatusBar to pick-up previously commited fixes.

--- softwarecenter/view/appdetailsview_gtk.py
+++ softwarecenter/view/appdetailsview_gtk.py
@@ -71,7 +71,8 @@ COLOR_BLACK         = '#323232'
 
 LOG = logging.getLogger("softwarecenter.view.appdetailsview")
 
-class PackageStatusBar(gtk.Alignment):
+
+class StatusBar(gtk.Alignment):
     
     def __init__(self, view):
         gtk.Alignment.__init__(self, xscale=1.0, yscale=1.0)
@@ -85,24 +86,12 @@ class PackageStatusBar(gtk.Alignment):
         self.add(self.hbox)
 
         self.view = view
-        self.label = mkit.EtchedLabel()
-        self.button = gtk.Button()
-        self.progress = gtk.ProgressBar()
-
-        self.pkg_state = None
-
-        self.hbox.pack_start(self.label, False)
-        self.hbox.pack_end(self.button, False)
-        self.hbox.pack_end(self.progress, False)
-        self.show_all()
 
         self._height = 1
 
         self.connect('size-allocate', self._on_size_allocate)
         self.connect('style-set', self._on_style_set)
-        self.button.connect('clicked', self._on_button_clicked)
-        return
-
+        
     def _on_size_allocate(self, button, allocation):
         # Bug #617443
         # Dont allow the package status bar to shrink
@@ -115,6 +104,47 @@ class PackageStatusBar(gtk.Alignment):
         # reset max heights, this is so we can resize properly on, say, a font-size change
         self._height = 1
         self.set_size_request(-1, -1)
+        return
+        
+    def draw(self, cr, a, expose_area):
+        if mkit.not_overlapping(a, expose_area): return
+
+        cr.save()
+        if self.view.section:
+            r,g,b = self.view.section._section_color
+        else:
+            r,g,b = 0.5,0.5,0.5
+
+        cr.rectangle(a)
+        cr.set_source_rgba(r,g,b,0.333)
+#        cr.set_source_rgb(*mkit.floats_from_string(self.line_color))
+        cr.fill()
+
+        cr.set_line_width(1)
+        cr.translate(0.5, 0.5)
+        cr.rectangle(a.x, a.y, a.width-1, a.height-1)
+        cr.set_source_rgba(r,g,b,0.5)
+        cr.stroke()
+        cr.restore()
+        return
+
+
+class PackageStatusBar(StatusBar):
+    
+    def __init__(self, view):
+        StatusBar.__init__(self, view)
+        self.label = mkit.EtchedLabel()
+        self.button = gtk.Button()
+        self.progress = gtk.ProgressBar()
+
+        self.pkg_state = None
+
+        self.hbox.pack_start(self.label, False)
+        self.hbox.pack_end(self.button, False)
+        self.hbox.pack_end(self.progress, False)
+        self.show_all()
+
+        self.button.connect('clicked', self._on_button_clicked)
         return
 
     def _on_button_clicked(self, button):
@@ -262,28 +292,6 @@ class PackageStatusBar(gtk.Alignment):
             self.line_color = COLOR_YELLOW_OUTLINE
         if self.app_details.warning and not self.app_details.error:
             self.set_label(self.app_details.warning)
-        return
-
-    def draw(self, cr, a, expose_area):
-        if mkit.not_overlapping(a, expose_area): return
-
-        cr.save()
-        if self.view.section:
-            r,g,b = self.view.section._section_color
-        else:
-            r,g,b = 0.5,0.5,0.5
-
-        cr.rectangle(a)
-        cr.set_source_rgba(r,g,b,0.333)
-#        cr.set_source_rgb(*mkit.floats_from_string(self.line_color))
-        cr.fill()
-
-        cr.set_line_width(1)
-        cr.translate(0.5, 0.5)
-        cr.rectangle(a.x, a.y, a.width-1, a.height-1)
-        cr.set_source_rgba(r,g,b,0.5)
-        cr.stroke()
-        cr.restore()
         return
 
 
@@ -894,29 +902,17 @@ class AddonsTable(gtk.VBox):
         self.show_all()
         return False
 
-class AddonsStatusBar(gtk.Alignment):
+class AddonsStatusBar(StatusBar):
     
     def __init__(self, addons_manager):
-        gtk.Alignment.__init__(self, xscale=1.0, yscale=1.0)
+        StatusBar.__init__(self, addons_manager.view)
         self.addons_manager = addons_manager
         self.addons_table = self.addons_manager.table
         self.cache = self.addons_manager.view.cache
-        self.view = self.addons_manager.view
-        
-        self.set_redraw_on_allocate(False)
-        self.set_padding(mkit.SPACING_SMALL,
-                         mkit.SPACING_SMALL,
-                         mkit.SPACING_SMALL+2,
-                         mkit.SPACING_SMALL)
-        
-        self.hbox = gtk.HBox(spacing=mkit.SPACING_SMALL)
-        self.add(self.hbox)
-
 
         self.applying = False
         
-        self.label_price = gtk.Label(_("Free"))
-        self.label_price.set_line_wrap(True)
+        self.label_price = mkit.EtchedLabel(_("Free"))
         self.hbox.pack_start(self.label_price, False)
         
         self.hbuttonbox = gtk.HButtonBox()
@@ -935,31 +931,10 @@ class AddonsStatusBar(gtk.Alignment):
             self.hide()
         else:
             self.show()
-
-    def draw(self, cr, a, expose_area):
-        if mkit.not_overlapping(a, expose_area): return
-
-        cr.save()
-        if self.view.section:
-            r,g,b = self.view.section._section_color
-        else:
-            r,g,b = 0.5,0.5,0.5
-
-        cr.rectangle(a)
-        cr.set_source_rgba(r,g,b,0.333)
-#        cr.set_source_rgb(*mkit.floats_from_string(self.line_color))
-        cr.fill()
-
-        cr.set_line_width(1)
-        cr.translate(0.5, 0.5)
-        cr.rectangle(a.x, a.y, a.width-1, a.height-1)
-        cr.set_source_rgba(r,g,b,0.5)
-        cr.stroke()
-        cr.restore()
-        return
     
     def get_applying(self):
         return self.applying
+
     def set_applying(self, applying):
         self.applying = applying
     
@@ -971,7 +946,8 @@ class AddonsStatusBar(gtk.Alignment):
         self.view.addons_to_install = self.addons_manager.addons_to_install
         self.view.addons_to_remove = self.addons_manager.addons_to_remove
         AppDetailsViewBase.apply_changes(self.view)
-        
+
+
 class AddonsManager():
     def __init__(self, view):
         self.view = view
