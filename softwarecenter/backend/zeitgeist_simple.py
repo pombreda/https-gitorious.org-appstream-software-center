f626ca9cc7f125f377965b98d5e9559008f5c4b4
Seif Lotfy <seif@lotfy.com>
2010-09-28 18:41:42 +0200

    add missing library

---
+++ softwarecenter/backend/zeitgeist_simple.py
@@ -0,0 +1,22 @@
+import sys
+from zeitgeist.client import ZeitgeistClient, ZeitgeistDBusInterface
+from zeitgeist.datamodel import Event, ResultType, Interpretation, TimeRange, Subject
+
+CLIENT = ZeitgeistClient()
+
+class ZeitgeistWrapper():
+    def __init__(self):
+        pass
+        
+    def get_usage_counter(self, application, callback):
+        application = "application://"+application.split("/")[-1]
+        def _callback(event_ids):
+            callback(len(event_ids))
+        e = Event()
+        e.actor = application
+        CLIENT.find_event_ids_for_templates([e], _callback, num_events = sys.maxint)
+        
+zeitgeist = ZeitgeistWrapper()
+
+
+
