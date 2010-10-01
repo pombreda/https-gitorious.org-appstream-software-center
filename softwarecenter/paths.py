6bda0fbce4bb9240ea2bb2be49dfb16669071db6
Michael Vogt <michael.vogt@ubuntu.com>
2010-10-01 10:39:54 +0200

    * softwarecenter/paths.py   - ensure we don't create directories in /home when running as   - When detecting a not writable cache dir, try to remove it     so that it gets re-created later with the right permissions.     That will undo the damage in bug LP: #652151

--- softwarecenter/paths.py
+++ softwarecenter/paths.py
@@ -16,9 +16,22 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import logging
 import os
 
 from xdg import BaseDirectory as xdg
+
+# there was a bug in maverick 3.0.3 (#652151) that could lead to a empty
+# root owned directory in ~/.cache/software-center - we remove it here
+# so that it gets later re-created with the right permissions
+def try_to_fixup_root_owned_dir_via_remove(directory):
+    if os.path.exists(directory) and not os.access(directory, os.W_OK):
+        try:
+            logging.warn("trying to fix not writable cache directory")
+            os.rmdir(directory)
+        except:
+            logging.exception("failed to fix not writable cache directory")
+
 
 # ensure we don't create directories in /home/$user
 if os.getuid() == 0 and "SUDO_USER" in os.environ and "HOME" in os.environ:
@@ -26,5 +39,8 @@ if os.getuid() == 0 and "SUDO_USER" in os.environ and "HOME" in os.environ:
 
 SOFTWARE_CENTER_CONFIG_DIR = os.path.join(xdg.xdg_config_home, "software-center")
 SOFTWARE_CENTER_CACHE_DIR = os.path.join(xdg.xdg_cache_home, "software-center")
+# FIXUP a brief broken software-center in maverick
+try_to_fixup_root_owned_dir_via_remove(SOFTWARE_CENTER_CACHE_DIR)
+
 SOFTWARE_CENTER_CONFIG_FILE = os.path.join(SOFTWARE_CENTER_CONFIG_DIR, "softwarecenter.cfg") 
 SOFTWARE_CENTER_ICON_CACHE_DIR = os.path.join(SOFTWARE_CENTER_CACHE_DIR, "icons")
