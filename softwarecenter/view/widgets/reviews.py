d0feea2fafd15473d3ce5af8b06291c4edd4ff97
Michael Vogt <michael.vogt@ubuntu.com>
2010-10-14 10:10:05 +0200

    softwarecenter/view/widgets/reviews.py: add simple stat widget

---
+++ softwarecenter/view/widgets/reviews.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Canonical
+#
+# Authors:
+#  Michael Vogt
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import pygtk
+pygtk.require ("2.0")
+import gobject
+import gtk
+import os
+
+class ReviewStatsContainer(gtk.HBox):
+
+    STAR_IMAGE = "star-yellow"
+    DARK_STAR_IMAGE = "star-dark"
+
+    ICON_SIZE = gtk.ICON_SIZE_MENU
+
+    def __init__(self, avg_rating, nr_reviews, icon_cache=None):
+        gtk.HBox.__init__(self)
+        self.avg_rating = avg_rating
+        self.nr_reviews = nr_reviews
+        if not icon_cache:
+            icons = gtk.icon_theme_get_default()
+            icons.append_search_path("/usr/share/software-center/images/")
+        else:
+            icons = icon_cache
+        for i in range(1,6):
+            name = "image_review_star%i" % i
+            setattr(self, name, gtk.Image())
+            self.pack_start(getattr(self, name), False, False)
+        self.label = gtk.Label("")
+        self.pack_start(self.label, False, False)
+        self._update_nr_reviews()
+        self._update_rating()
+    def _update_nr_reviews(self):
+        self.label.set_markup("<small>(%s)</small>" % self.nr_reviews)
+    def _update_rating(self):
+        for i in range(1, self.avg_rating+1):
+            img = getattr(self, "image_review_star%i" % i)
+            img.set_from_icon_name(self.STAR_IMAGE, self.ICON_SIZE)
+        for i in range(self.avg_rating+1, 6):
+            img = getattr(self, "image_review_star%i" % i)
+            img.set_from_icon_name(self.DARK_STAR_IMAGE, self.ICON_SIZE)
+
+if __name__ == "__main__":
+    icons = gtk.icon_theme_get_default()
+    icons.append_search_path("/usr/share/software-center/images/")
+    if os.path.exists("data/images/"):
+        icons.append_search_path("data/images")
+    w = ReviewStatsContainer(3.5, 101, icon_cache=icons)
+    w.show_all()
+    win = gtk.Window()
+    win.add(w)
+    win.show()
+
+    gtk.main()
