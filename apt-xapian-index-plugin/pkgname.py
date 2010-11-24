683daabc4e9887e9cc72622b875217187603cb5d
Michael Vogt <michael.vogt@ubuntu.com>
2010-11-24 15:33:22 +0100

    apt-xapian-index-plugin/pkgname.py: add axi plugin that stores the pkgname as a value. the docs say that get_data() is slower than get_value() - I did not find much of a difference on my (fast) system though)

---
+++ apt-xapian-index-plugin/pkgname.py
@@ -0,0 +1,95 @@
+# Add origin tags to the index
+
+import apt
+import re
+import os
+
+class PkgnamePlugin:
+
+    def info(self):
+        """
+        Return general information about the plugin.
+
+        The information returned is a dict with various keywords:
+         
+         timestamp (required)
+           the last modified timestamp of this data source.  This will be used
+           to see if we need to update the database or not.  A timestamp of 0
+           means that this data source is either missing or always up to date.
+         values (optional)
+           an array of dicts { name: name, desc: description }, one for every
+           numeric value indexed by this data source.
+
+        Note that this method can be called before init.  The idea is that, if
+        the timestamp shows that this plugin is currently not needed, then the
+        long initialisation can just be skipped.
+        """
+        file = apt.apt_pkg.config.find_file("Dir::Cache::pkgcache")
+        if not os.path.exists(file):
+            return dict(timestamp = 0)
+        return dict(timestamp = os.path.getmtime(file),
+                    values = [dict(name= "pkgname", desc="Pkgname as value"),
+                              ])
+
+    def init(self, info, progress):
+        """
+        If needed, perform long initialisation tasks here.
+
+        info is a dictionary with useful information.  Currently it contains
+        the following values:
+
+          "values": a dict mapping index mnemonics to index numbers
+
+        The progress indicator can be used to report progress.
+        """
+        values = info['values']
+        self.value = values.get("pkgname", -1)
+
+    def doc(self):
+        """
+        Return documentation information for this data source.
+
+        The documentation information is a dictionary with these keys:
+          name: the name for this data source
+          shortDesc: a short description
+          fullDoc: the full description as a chapter in ReST format
+        """
+        return dict(
+            name = "Pkgname",
+            shortDesc = "Add a value with the pkgname to the DB",
+            fullDoc = """
+            Make the pkgname accessible via a "value". This is useful
+            for e.g. MatchDeciders because get_value() is quicker
+            than "get_data()" (says the docs)
+            """
+        )
+
+
+    def index(self, document, pkg):
+        """
+        Update the document with the information from this data source.
+
+        document  is the document to update
+        pkg       is the python-apt Package object for this package
+        """
+        document.add_value(self.value, pkg.name)
+
+    def indexDeb822(self, document, pkg):
+        """
+        Update the document with the information from this data source.
+
+        This is alternative to index, and it is used when indexing with package
+        data taken from a custom Packages file.
+
+        document  is the document to update
+        pkg       is the Deb822 object for this package
+        """
+        # XXX
+        return
+
+
+def init():
+    """
+    Create and return the plugin object.
+    """
+    return PkgnamePlugin()
