6cec4e15ce1ef3bfa0777e9d4ccf85d996b989d6
Gary Lasker <gary.lasker@canonical.com>
2011-01-26 13:29:34 -0500

    fix ZeroDivisionError when navigating to the details view for an item that does not yet have a single review (note also that the review stars on not displayed in this error case)

--- softwarecenter/view/appdetailsview_gtk.py
+++ softwarecenter/view/appdetailsview_gtk.py
@@ -1404,7 +1404,10 @@ class AppDetailsViewGtk(gtk.Viewport, AppDetailsViewBase):
         # wrong
         stats = ReviewStats(app)
         stats.ratings_total = len(reviews)
-        stats.ratings_average = sum([x.rating for x in reviews]) / float(stats.ratings_total)
+        if stats.ratings_total == 0:
+            stats.ratings_average = 0
+        else:
+            stats.ratings_average = sum([x.rating for x in reviews]) / float(stats.ratings_total)
         self._update_review_stats_widget(stats)
         self.reviews.finished()
 
