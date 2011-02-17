55fe918c5d9362d4d388bb35e7e81d413b2f7682
Michael Vogt <michael.vogt@ubuntu.com>
2011-02-17 16:52:48 +0100

    * softwarecenter/db/reviews.py, test/test_reviews.py:   - sort reviews by version/usefulness/date

--- softwarecenter/view/appdetailsview_gtk.py
+++ softwarecenter/view/appdetailsview_gtk.py
@@ -1070,12 +1070,8 @@ class UIReviewsList(gtk.VBox):
     def _fill(self):
         # FIXME: this should go away (or into a different method),
         #        we want the fill from the reviews-ready callback
+        #  duplicated code
         if self.reviews:
-
-            self.reviews.reverse()  # XXX: sort so that reviews are ordered recent to oldest
-                                    # XXX: prob should be done somewhere else, perhaps in the review fetcher?
-                                    # mvo: the server will eventually sort for us (hopefully) by usefulness/version/date, but for now looks like we need to do it
-
             for r in self.reviews:
                 pkgversion = self._parent.app_details.version
                 review = UIReview(r, pkgversion, self.logged_in_person)
