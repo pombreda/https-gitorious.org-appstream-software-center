3237e9b7908a7f34714a649c5a1147d72da72673
Michael Vogt <michael.vogt@ubuntu.com>
2011-03-07 11:47:44 +0100

    test/test_catview.py: add test for lobbyview

--- test/test_catview.py
+++ test/test_catview.py
@@ -8,9 +8,35 @@ import unittest
 sys.path.insert(0,"../")
 from softwarecenter.apt.aptcache import AptCache
 from softwarecenter.db.database import StoreDatabase
-from softwarecenter.view.catview_gtk import SubCategoryViewGtk
+from softwarecenter.view.catview_gtk import LobbyViewGtk, SubCategoryViewGtk
 from softwarecenter.paths import *
 from softwarecenter.distro import get_distro
+
+class TestLobbyViewGtk(unittest.TestCase):
+
+    def setUp(self):
+        datadir = "../data"
+        self.desktopdir = "/usr/share/app-install/"
+        cache = AptCache()
+        cache.open()
+        xapian_base_path = XAPIAN_BASE_PATH
+        pathname = os.path.join(xapian_base_path, "xapian")
+        db = StoreDatabase(pathname, cache)
+        db.open()
+        distro = get_distro()
+        # icon mock
+        icons = gtk.icon_theme_get_default()
+        # create a details object
+        self.lobbyview = LobbyViewGtk(
+            datadir, self.desktopdir, cache, db, icons, None)
+        self.lobbyview.show_all()
+
+    def test_categories_in_view(self):
+        self.assertTrue(
+            self.lobbyview.whatsnew_carousel.get_property("visible"))
+        self.assertTrue(
+            self.lobbyview.featured_carousel.get_property("visible"))
+        self.assertEqual(self.lobbyview.header, "Departments")
 
 class TestSubCatViewGtk(unittest.TestCase):
 
