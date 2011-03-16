a651121b9a65c4abfafb999dadfe2a76ee631a88
Gary Lasker <gary.lasker@canonical.com>
2011-03-16 16:12:02 -0400

    remove fixme

--- softwarecenter/utils.py
+++ softwarecenter/utils.py
@@ -315,7 +315,6 @@ def convert_desktop_file_to_installed_location(app_install_data_file_path):
     """ returns the installed desktop file path that corresponds to the
         given app-install-date file path
     """
-    # FIXME:  handle all edge cases e.g. kde4 in subdirectory, etc.
     installed_desktop_file_path = app_install_data_file_path.replace("app-install/desktop", "applications")
     # handle case where a subdirectory is encoded in the app-install
     # desktop filename, e.g. kde4_soundkonverter.desktop
