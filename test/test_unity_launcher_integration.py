2697d6df58c2c0d231f8deb13e4b525b523979c8
Michael Vogt <michael.vogt@ubuntu.com>
2011-04-20 12:05:01 +0200

    improve tests for the launcher integration

--- test/test_unity_launcher_integration.py
+++ test/test_unity_launcher_integration.py
@@ -14,7 +14,7 @@ sys.path.insert(0, "..")
 import softwarecenter.utils
 from softwarecenter.app import SoftwareCenterApp
 from softwarecenter.paths import XAPIAN_BASE_PATH
-from softwarecenter.enums import ACTION_BUTTON_ADD_TO_LAUNCHER
+from softwarecenter.enums import ACTION_BUTTON_ADD_TO_LAUNCHER, TRANSACTION_TYPE_INSTALL
 from softwarecenter.utils import convert_desktop_file_to_installed_location
 from softwarecenter.view.appview import AppStore
 from softwarecenter.db.application import Application
@@ -34,6 +34,11 @@ s_c_app.window_main.show_all()
 
 class TestUnityLauncherIntegration(unittest.TestCase):
     
+    def _zzz(self):
+        for i in range(10):
+            time.sleep(0.1)
+            self._p()
+
     def _p(self):
         while gtk.events_pending():
             gtk.main_iteration()
@@ -42,6 +47,13 @@ class TestUnityLauncherIntegration(unittest.TestCase):
         self.s_c_app = s_c_app
         # monkey patch is_unity_running
         softwarecenter.utils.is_unity_running = lambda: True
+        self.s_c_app.available_pane._send_dbus_signal_to_unity_launcher = Mock()
+
+    def _reset_ui(self):
+        self.s_c_app.available_pane.navigation_bar.remove_all(animate=False)
+        self._p()
+        time.sleep(0.5)
+        self._p()
 
     def _run_search(self, search_text):
         logging.info("_run_search", search_text)
@@ -58,24 +70,50 @@ class TestUnityLauncherIntegration(unittest.TestCase):
             pkgname_from_row, needle, "expected row '%s' got '%s'" % (
                 needle, pkgname_from_row))
 
-    def test_unity_launcher_integration(self):
-        # test package is the inimitable lincity-ng
-        # Note: this test relies on lincity-ng being *not installed*
-        #       on the test machine!
-        pkgname = "lincity-ng"
-        model = self._run_search(pkgname)
-        self.assertFirstPkgInModel(model, pkgname)
-        treeview = self.s_c_app.available_pane.app_view
-        treeview.row_activated(model.get_path(model.get_iter_root()),
-                               treeview.get_column(0))
+    def _navigate_to_pkgname_and_click_install(self, pkgname):
+        self._reset_ui()
+        self.s_c_app.show_available_packages([pkgname])
         self._p()
-
-        # click the "Install" button
-        self.s_c_app.available_pane.app_details_view.pkg_statusbar.button.clicked()
-        for i in range(2):
-            self._p()
-            time.sleep(0.3)
+        appname = self.s_c_app.available_pane.app_details_view.app.appname
+        # pretend we started a install
+        self.s_c_app.backend.emit("transaction-started", pkgname, appname, "testid101", TRANSACTION_TYPE_INSTALL)
+        # wait a wee bit
+        self._zzz()
         
+    def test_unity_launcher_stays_after_install_finished(self):
+        pkgname = "gl-117"
+        mock_result = Mock()
+        mock_result.pkgname = pkgname
+        mock_result.success = True
+        # now pretend
+        self._navigate_to_pkgname_and_click_install(pkgname)
+        # pretend we are done
+        self.s_c_app.backend.emit("transaction-finished", mock_result)
+        # this is normally set in the transaction-finished call but our
+        # app is not really installed so we need to mock it here
+        self.s_c_app.available_pane.unity_launcher_items[pkgname].installed_desktop_file_path = "/some/path"
+        # wait a wee bit
+        self._zzz()
+        # ensure we still have the button
+        button = self.s_c_app.available_pane.action_bar.get_button(
+            ACTION_BUTTON_ADD_TO_LAUNCHER)
+        self.assertNotEqual(button, None)
+        self.assertTrue(button.get_property("visible"))
+        # ensure we haven't called the launcher prematurely
+        self.assertFalse(self.s_c_app.available_pane._send_dbus_signal_to_unity_launcher.called)
+        # now click it and ensure its added even though the transaction is over
+        button.clicked()
+        self._zzz()
+        # ensure the button is gone
+        button = self.s_c_app.available_pane.action_bar.get_button(
+            ACTION_BUTTON_ADD_TO_LAUNCHER)
+        self.assertEqual(button, None)
+        self.assertTrue(self.s_c_app.available_pane._send_dbus_signal_to_unity_launcher.called)
+
+
+    def test_unity_launcher_integration(self):
+        pkgname = "lincity-ng"
+        self._navigate_to_pkgname_and_click_install(pkgname)
         # verify that the panel is shown offering to add the app to the launcher
         self.assertTrue(
             self.s_c_app.available_pane.action_bar.get_property("visible"))
@@ -89,13 +127,13 @@ class TestUnityLauncherIntegration(unittest.TestCase):
         self.assertTrue(pkgname in self.s_c_app.available_pane.unity_launcher_items)
         launcher_info = self.s_c_app.available_pane.unity_launcher_items.pop(pkgname)
         # check the UnityLauncherInfo values themselves
-        self.assertEqual(launcher_info.name, u"Lincity-NG")
+        self.assertEqual(launcher_info.name, "Lincity-ng")
         self.assertEqual(launcher_info.icon_name, "lincity-ng")
         self.assertTrue(launcher_info.icon_x > 20)
         self.assertTrue(launcher_info.icon_y > 20)
         self.assertEqual(launcher_info.icon_size, 84)
         self.assertEqual(launcher_info.app_install_desktop_file_path, "/usr/share/app-install/desktop/lincity-ng.desktop")
-        self.assertTrue(launcher_info.trans_id.startswith("/org/debian/apt/transaction/"))
+        self.assertEqual(launcher_info.trans_id, "testid101")
         # finally, make sure the the app has been removed from the launcher queue        
         self.assertFalse(pkgname in self.s_c_app.available_pane.unity_launcher_items)
         
