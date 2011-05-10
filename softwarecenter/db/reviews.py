908c93d41bbfc55eab18487798fbdc6eec52db2b
Michael Vogt <michael.vogt@ubuntu.com>
2011-05-10 15:35:36 +0200

    add page support to the helpers

--- softwarecenter/db/reviews.py
+++ softwarecenter/db/reviews.py
@@ -180,7 +180,7 @@ class ReviewLoader(object):
                 LOG.exception("review stats cache load failure")
                 os.rename(self.REVIEW_STATS_CACHE_FILE, self.REVIEW_STATS_CACHE_FILE+".fail")
 
-    def get_reviews(self, application, callback):
+    def get_reviews(self, application, callback, page=1):
         """run callback f(app, review_list) 
            with list of review objects for the given
            db.database.Application object
@@ -359,7 +359,7 @@ class ReviewLoaderSpawningRNRClient(ReviewLoader):
         self.rnrclient._offline_mode = not network_state_is_connected()
 
     # reviews
-    def get_reviews(self, translated_app, callback):
+    def get_reviews(self, translated_app, callback, page=1):
         """ public api, triggers fetching a review and calls callback
             when its ready
         """
@@ -391,6 +391,7 @@ class ReviewLoaderSpawningRNRClient(ReviewLoader):
                "--origin", origin, 
                "--distroseries", distroseries, 
                "--pkgname", str(app.pkgname), # ensure its str, not unicode
+               "--page", str(page),
               ]
         try:
             (pid, stdin, stdout, stderr) = glib.spawn_async(
@@ -519,7 +520,7 @@ class ReviewLoaderJsonAsync(ReviewLoader):
         # run callback
         callback(app, sorted(reviews, reverse=True))
 
-    def get_reviews(self, app, callback):
+    def get_reviews(self, app, callback, page=1):
         """ get a specific review and call callback when its available"""
         # FIXME: get this from the app details
         origin = self.cache.get_origin(app.pkgname)
@@ -590,7 +591,7 @@ class ReviewLoaderFake(ReviewLoader):
         return random.choice(self.LOREM.split("\n\n"))
     def _random_summary(self):
         return random.choice(self.SUMMARIES)
-    def get_reviews(self, application, callback):
+    def get_reviews(self, application, callback, page=1):
         if not application in self._review_stats_cache:
             self.get_review_stats(application)
         stats = self._review_stats_cache[application]
