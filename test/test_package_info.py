18320b3bb2a04431e2777551978b1e0af80c303e
Alex Eftimie <alex@eftimie.ro>
2011-05-31 17:08:59 +0300

    experimental PackageInfo Package abstraction - a few tests

--- test/test_package_info.py
+++ test/test_package_info.py
@@ -22,6 +22,22 @@ class TestPkgInfo(unittest.TestCase):
         self.assertEqual(pkginfo.get_section('bash'), 'shells')
         self.assertEqual(pkginfo.get_summary('bash'), 'The GNU Bourne Again SHell')
         self.assertTrue(pkginfo.get_description('bash') != '')
+        self.assertTrue(len(pkginfo.get_origins("firefox")) > 0)
+        self.assertTrue(pkginfo.get_installed("coreutils") is not None)
+        self.assertTrue(pkginfo.get_candidate("coreutils") is not None)
+        self.assertTrue(len(pkginfo.get_available("coreutils")) != 0)
+
+        self.assertTrue('coreutils' in pkginfo)
+        
+        pkg = pkginfo['coreutils']
+        self.assertTrue(pkg is not None)
+        self.assertTrue(pkg.is_installed)
+        for p in ('section', 'summary', 'description', 'origins'):
+            self.assertTrue(p in dir(pkg))
+        self.assertTrue(len(pkg.origins) != 0)
+        self.assertEqual(pkg.section, "utils")
+        self.assertTrue(pkg.summary != '')
+        self.assertTrue(pkg.description != '')
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
