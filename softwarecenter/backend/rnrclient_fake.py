e78cf1554671648eef4b41d2d5637aa01bd81835
Aaron Peachey <alpeachey@gmail.com>
2011-06-02 20:16:22 +1000

    add some more fake functionality and document the fake API

--- softwarecenter/backend/rnrclient_fake.py
+++ softwarecenter/backend/rnrclient_fake.py
@@ -23,14 +23,18 @@ from rnrclient_pristine import ReviewRequest, ReviewsStats, ReviewDetails
 from test.fake_review_settings import FakeReviewSettings
 import piston_mini_client
 import simplejson
+import random
+import time
 
 class RatingsAndReviewsAPI(PistonAPI):
-    """A client for talking to the reviews and ratings API.
-
-    If you pass no arguments into the constructor it will try to connect to
-    localhost:8000 so you probably want to at least pass in the
-    ``service_root`` constructor argument.
+    """A fake client pretending to be RAtingsAndReviewsAPI from rnrclient_pristine.
+       Uses settings from the class in test.fake_review_settings. FakeReviewSettings
+       to provide predictable responses to methods that try to use the 
+       RatingsAndReviewsAPI for testing purposes (i.e. without network activity).
+       To use this, instead of importing from rnrclient_pristine, you can import
+       from rnrclient_fake instead.
     """
+    
     default_service_root = 'http://localhost:8000/reviews/api/1.0'
     default_content_type = 'application/x-www-form-urlencoded'
     exception_msg = 'Fake RatingsAndReviewsAPI raising fake exception'
@@ -74,36 +78,18 @@ class RatingsAndReviewsAPI(PistonAPI):
         if FakeReviewSettings.get_reviews_error:
             raise APIError(self.exception_msg)
         else:
-            if FakeReviewSettings.reviews_returned == 0:
-                return simplejson.dumps([])
-            else:
-                return simplejson.dumps([
-                    {
-                        "origin": "ubuntu",
-                        "rating": 4,
-                        "hide": False,
-                        "app_name": "",
-                        "language": "en",
-                        "reviewer_username": "timfake",
-                        "usefulness_total": 0,
-                        "usefulness_favorable": 0,
-                        "review_text": "review text",
-                        "date_deleted": None,
-                        "summary": "tdgfstmodiofied",
-                        "version": "1:0.9.4+bzr20110415-0ubuntu2",
-                        "id": 6,
-                        "date_created": "2011-05-28 12:04:54",
-                        "reviewer_displayname": "Tim Fake",
-                        "package_name": "compiz-core",
-                        "distroseries": "natty"
-                    }
-                    ])
+            reviews = self._make_fake_reviews(packagename, 
+                                              FakeReviewSettings.reviews_returned)
+            return simplejson.dumps(reviews)
 
     @validate('review_id', int)
     @returns(ReviewDetails)
     def get_review(self, review_id):
-        """Fetch a particular review via its id."""
-        #return self._get('reviews/%s/' % review_id)
+        if FakeReviewSettings.get_review_error:
+            raise APIError(self.exception_msg)
+        else:
+            review = self._make_fake_reviews(single_id=review_id)
+            return simplejson.dumps(review)
 
     @validate('review', ReviewRequest)
     @returns(ReviewDetails)
@@ -149,3 +135,48 @@ class RatingsAndReviewsAPI(PistonAPI):
 
         #return self._get('usefulness/', args=data,
         #    scheme=PUBLIC_API_SCHEME)
+        
+        
+    def _make_fake_reviews(self, packagename='compiz-core', 
+                           quantity=1, single_id=None):
+        """Make and return a requested quantity of fake reviews"""
+        USERS = ["Joe Doll", "John Foo", "Cat Lala", "Foo Grumpf", 
+                 "Bar Tender", "Baz Lightyear"]
+        SUMMARIES = ["Cool", "Medium", "Bad", "Too difficult"]
+        TEXT = ["Review text number 1", "Review text number 2", 
+                "Review text number 3", "Review text number 4"]
+        
+        reviews = []
+        
+        for i in range(0, quantity):
+            if quantity == 1 and single_id:
+                id = single_id
+            else:
+                id = 1*3
+                
+            r = {
+                        "origin": "ubuntu",
+                        "rating": random.randint(1,5),
+                        "hide": False,
+                        "app_name": "",
+                        "language": "en",
+                        "reviewer_username": random.choice(USERS),
+                        "usefulness_total": random.randint(3,6),
+                        "usefulness_favorable": random.randint(1,3),
+                        "review_text": random.choice(TEXT),
+                        "date_deleted": None,
+                        "summary": random.choice(SUMMARIES),
+                        "version": "1:0.9.4",
+                        "id": id,
+                        "date_created": time.strftime("%Y-%m-%d %H:%M:%S"),
+                        "reviewer_displayname": "Fake Person",
+                        "package_name": packagename,
+                        "distroseries": "natty"
+            }
+            reviews.append(r)
+            
+        #get_review wants a dict but get_reviews wants a list of dicts
+        if single_id:
+            return r
+        else:
+            return reviews
--- test/fake_review_settings.py
+++ test/fake_review_settings.py
@@ -1,23 +1,32 @@
 
 class FakeReviewSettings(object):
     #server status 
-    server_response_ok = True
+    #*************
+    server_response_ok = True     #raises APIError if false
     
     #review stats
-    review_stats_error = False
+    #*************
+    review_stats_error = False    #raises APIError if true
     
     #get reviews
-    get_reviews_error= False
-    reviews_returned = 1
+    #*************
+    get_reviews_error= False   #raises APIError if true
+
+    #the following has no effect if get_reviews_error = True
+    reviews_returned = 1       #determines number of reviews to return 
     
     #get review
-    get_review_error= False
+    #*************
+    get_review_error= False   #raises APIError if true
     
     #flag review
-    flag_review_error = False
+    #*************
+    flag_review_error = False   #raises APIError if true
     
     #submit usefulness
-    submit_usefulness_error = False
+    #*************
+    submit_usefulness_error = False   #raises APIError if true
     
     #get usefulness
-    get_usefulness_error = False
+        #*************
+    get_usefulness_error = False   #raises APIError if true
