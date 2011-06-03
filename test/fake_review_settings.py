e049fb7bd7a4adcfe2a7812ae697556de84481cd
Aaron Peachey <alpeachey@gmail.com>
2011-06-03 10:43:06 +1000

    update some documentation

--- test/fake_review_settings.py
+++ test/fake_review_settings.py
@@ -1,12 +1,25 @@
 
 class FakeReviewSettings(object):
-    #server status 
-    #*************
+    '''An object that simply holds settings which are used by RatingsAndReviewsAPI
+       in the rnrclient_fake module. Using this module allows a developer to test
+       the reviews functionality without any interaction with a reviews server.
+       Each setting here provides complete control over how the 'server' will 
+       respond. Changes to these settings should be made to the class attributes
+       directly without creating an instance of this class.
+       The intended usage is for unit tests where a predictable response is 
+       required and where the application should THINK it has spoken to a 
+       server. 
+       The unit test would make changes to settings in this class before 
+       running the unit test.
+    '''
+
+    #server status
+    #*****************************
     #raises APIError if True
     server_response_error = False
     
     #review stats
-    #*************
+    #*****************************
     #raises APIError if True
     review_stats_error = False    
     
@@ -16,7 +29,7 @@ class FakeReviewSettings(object):
     packages_returned = 3
     
     #get reviews
-    #*************
+    #*****************************
     #raises APIError if True
     get_reviews_error= False
 
@@ -25,17 +38,17 @@ class FakeReviewSettings(object):
     reviews_returned = 1
     
     #get review
-    #*************
+    #*****************************
     #raises APIError if True
     get_review_error= False
     
     #flag review
-    #*************
+    #*****************************
     #raises APIError if True
     flag_review_error = False
     
     #submit usefulness
-    #*************
+    #*****************************
     #raises APIError if True
     submit_usefulness_error = False
     
@@ -45,6 +58,6 @@ class FakeReviewSettings(object):
     usefulness_response_string = "Created"
     
     #get usefulness
-    #*************
+    #*****************************
     #raises APIError if True
     get_usefulness_error = False
