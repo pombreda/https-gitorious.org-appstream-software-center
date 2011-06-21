a7e7baa915e2a45120fbfe7fa4f3d77d4f7c64a9
Michael Vogt <michael.vogt@ubuntu.com>
2011-06-21 11:28:59 +0200

    add subscriptions_for_me and available_apps_qa

--- utils/piston-helpers/get_scagent_available_apps.py
+++ utils/piston-helpers/get_scagent_available_apps.py
@@ -3,13 +3,43 @@
 import os
 import pickle
 import logging
-
+import glib
 import argparse
+
+import piston_mini_client.auth
 
 from softwarecenter.paths import SOFTWARE_CENTER_CACHE_DIR
 from softwarecenter.backend.scaclient import SoftwareCenterAgentAPI
+from softwarecenter.backend.login_sso import get_sso_backend
+
+from gettext import gettext as _
 
 LOG = logging.getLogger(__name__)
+
+class SSOLoginHelper(object):
+    def __init__(self):
+        self.oauth = None
+        self.loop = glib.MainLoop(glib.main_context_default())
+    
+    def _login_successful(self, sso_backend, oauth_result):
+        self.oauth = oauth_result
+        # FIXME: actually verify the token against ubuntu SSO
+        self.loop.quit()
+
+    def get_oauth_token_sync(self):
+        # FIXME: support xid passing for the login stuff
+        xid = 0
+        sso = get_sso_backend(
+            xid, "Ubuntu Software Center Store",
+            _("To reinstall previous purchases, sign in to the "
+              "Ubuntu Single Sign-On account you used to pay for them."))
+        
+        sso.connect("login-successful", self._login_successful)
+        sso.connect("login-failed", lambda s: self.loop.quit())
+        sso.connect("login-canceled", lambda s: self.loop.quit())
+        sso.login_or_register()
+        self.loop.run()
+        return self.oauth
 
 if __name__ == "__main__":
     logging.basicConfig()
@@ -47,7 +77,19 @@ if __name__ == "__main__":
         cachedir = None
     else:
         cachedir = os.path.join(SOFTWARE_CENTER_CACHE_DIR, "scaclient")
-    scaclient = SoftwareCenterAgentAPI(cachedir=cachedir)
+
+
+    # check if auth is required
+    if args.command in ("available_apps_qa", "subscriptions_for_me"):
+        token = SSOLoginHelper().get_oauth_token_sync()
+        auth = piston_mini_client.auth.OAuthAuthorizer(token["token"],
+                                                       token["token_secret"],
+                                                       token["consumer_key"],
+                                                       token["consumer_secret"])
+        scaclient = SoftwareCenterAgentAPI(cachedir=cachedir, auth=auth)
+    else:
+        scaclient = SoftwareCenterAgentAPI(cachedir=cachedir)
+        
     piston_reply = None
 
     # common kwargs
@@ -56,16 +98,26 @@ if __name__ == "__main__":
                   "series": args.series,
                   "arch": args.arch
                   }
-        
+
     # handle the args
     if args.command == "available_apps":
         try:
             piston_reply = scaclient.available_apps(**kwargs)
         except:
-            LOG.exception("get_review_stats")
+            LOG.exception("available_apps")
+    elif args.command == "available_apps_qa":
+        try:
+            piston_reply = scaclient.available_apps_qa(**kwargs)
+        except:
+            LOG.exception("available_apps_qa")
+    elif args.command == "subscriptions_for_me":
+        try:
+            piston_reply = scaclient.subscriptions_for_me()
+        except:
+            LOG.exception("subscriptions_for_me")
 
     # print to stdout where its consumed by the parent
-    if piston_reply:
+    if piston_reply is not None:
         try:
             print pickle.dumps(piston_reply)
         except IOError:
