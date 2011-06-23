eada024ee514de14e3929377d20ef4867fdff012
Aaron Peachey <alpeachey@gmail.com>
2011-06-23 17:53:33 +1000

    *softwarecenter/utils.py: add utilities to   calculate expected lowest rating for top   rated apps, based on Wilson Score approach

--- softwarecenter/utils.py
+++ softwarecenter/utils.py
@@ -396,6 +396,53 @@ def get_person_from_config():
         return cfg.get("reviews", "username")
     return None
 
+def pnormaldist(qn):
+    '''Inverse normal distribution, based on the Ruby statistics2.pnormaldist'''
+    b = [1.570796288, 0.03706987906, -0.8364353589e-3,
+         -0.2250947176e-3, 0.6841218299e-5, 0.5824238515e-5,
+         -0.104527497e-5, 0.8360937017e-7, -0.3231081277e-8,
+         0.3657763036e-10, 0.6936233982e-12]
+        
+    if qn < 0 or qn > 1:
+        raise ValueError("qn must be between 0.0 and 1.0")
+    if qn == 0.5:
+        return 0.0
+    
+    w1 = qn
+    if qn > 0.5:
+        w1 = 1.0 - w1
+    w3 = -math.log(4.0 * w1 * (1.0 - w1))
+    w1 = b[0]
+    for i in range (1,11):
+        w1 = w1 + (b[i] * math.pow(w3, i))
+        
+    if qn > 0.5:
+        return math.sqrt(w1*w3)
+    else:
+        return -math.sqrt(w1*w3)
+
+def ci_lower_bound(pos, n, power):
+    if n == 0:
+        return 0
+    z = pnormaldist(1-power/2)
+    phat = 1.0 * pos / n
+    return (phat + z*z/(2*n) - z * math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
+
+def calc_erl(ratings):
+   if not len(ratings) == 5:
+      raise AttributeError('ratings argument must be a list of 5 integers')
+   
+   tot_ratings = 0
+   for i in range (0,5):
+      tot_ratings = ratings[i] + tot_ratings
+      
+   sum_scores = 0.0
+   for i in range (0,5):
+      wilson_score = ci_lower_bound(ratings[i],tot_ratings,0.10)
+      sum_scores = sum_scores + float(i+1)*wilson_score
+   
+   return sum_scores
+
 class SimpleFileDownloader(gobject.GObject):
 
     LOG = logging.getLogger("softwarecenter.simplefiledownloader")
