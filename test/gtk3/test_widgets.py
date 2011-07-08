9523969b0677c3eddd2e7f56ec33b7a1d24ac61b
Michael Vogt <michael.vogt@ubuntu.com>
2011-07-08 16:20:29 +0200

    add test for SimpleShowImageDialog and remove unused ShowImageDialog

--- test/gtk3/test_widgets.py
+++ test/gtk3/test_widgets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import glib
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf, GObject
 import mock
 import os
 import sys
@@ -16,7 +16,10 @@ from softwarecenter.ui.gtk3.widgets.actionbar import ActionBar
 from softwarecenter.ui.gtk3.widgets.backforward import get_test_backforward_window
 from softwarecenter.ui.gtk3.widgets.containers import get_test_container_window
 from softwarecenter.ui.gtk3.widgets.description import get_test_description_window
+from softwarecenter.ui.gtk3.widgets.imagedialog import SimpleShowImageDialog
 
+# window destory timeout
+TIMEOUT=100
 
 class TestStars(unittest.TestCase):
     """ tests the stars widget """
@@ -24,7 +27,7 @@ class TestStars(unittest.TestCase):
     def test_stars(self):
         win = get_test_stars_window()
         win.show_all()
-        glib.timeout_add_seconds(1, lambda: win.destroy())
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
         Gtk.main()
 
     def test_actionbar(self):
@@ -33,32 +36,37 @@ class TestStars(unittest.TestCase):
         actionbar.add_button("id1", "label", mock)
         actionbar.add_button("id2", "label", mock)
         actionbar.remove_button("id2")
-        actionbar.remove_button("id1")
         win = Gtk.Window()
         win.set_size_request(400, 400)
         win.add(actionbar)
         win.connect("destroy", Gtk.main_quit)
         win.show_all()
-        glib.timeout_add_seconds(1, lambda: win.destroy())
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
         Gtk.main()
 
     def test_backforward(self):
         win = get_test_backforward_window()
         win.show_all()
-        glib.timeout_add_seconds(1, lambda: win.destroy())
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
         Gtk.main()
 
     def test_containers(self):
         win = get_test_container_window()
         win.show_all()
-        glib.timeout_add_seconds(1, lambda: win.destroy())
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
         Gtk.main()
 
     def test_description(self):
         win = get_test_description_window()
         win.show_all()
-        glib.timeout_add_seconds(1, lambda: win.destroy())
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
         Gtk.main()
+
+    def test_show_image_dialog(self):
+        pix = GdkPixbuf.Pixbuf.new_from_file("../../data/images/dummy-screenshot-ubuntu.png")
+        d = SimpleShowImageDialog("test caption", pix)
+        GObject.timeout_add(TIMEOUT, lambda: d.destroy())
+        d.run()
 
 
 if __name__ == "__main__":
