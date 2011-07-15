2c8fc2d93117ddba2eff34c96d6e49aa0e996690
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2011-07-16 09:14:18 +1200

    add a transaction count to the progress icon

--- softwarecenter/ui/gtk3/widgets/symbolic_icons.py
+++ softwarecenter/ui/gtk3/widgets/symbolic_icons.py
@@ -3,9 +3,11 @@ import cairo
 from math import pi as PI
 from gi.repository import Gtk, Gdk, GObject
 
+from softwarecenter.ui.gtk3.drawing import rounded_rect
+
 # pi constants
 _2PI = 2 * PI
-PI_OVER_180 =   PI/180
+PI_OVER_180 =   PI / 180
 
 
 def radian(deg):
@@ -20,6 +22,7 @@ class SymbolicIcon(Gtk.Image):
 
     def __init__(self, name):
         Gtk.Image.__init__(self)
+        self.set_name("symbolic-icon")
 
         drop_shadow_path = self.SYMBOLIC_DIR + self.DROPSHADOW % name
         self.drop_shadow = cairo.ImageSurface.create_from_png(drop_shadow_path)
@@ -29,14 +32,8 @@ class SymbolicIcon(Gtk.Image):
         self.drop_shadow_x_offset = 0
         self.drop_shadow_y_offset = 1
 
-        normal_rgb = Gdk.RGBA()
-        active_rgb = Gdk.RGBA()
-        normal_rgb.parse("#DBDBDB")    # light grey
-        active_rgb.parse("#FFFFFF")    # white
-
         self.connect("draw", self.on_draw, self.drop_shadow, self.icon,
-                     self.drop_shadow_x_offset, self.drop_shadow_y_offset,
-                     normal_rgb, active_rgb)
+                     self.drop_shadow_x_offset, self.drop_shadow_y_offset)
         return
 
     def do_get_preferred_width(self):
@@ -47,21 +44,20 @@ class SymbolicIcon(Gtk.Image):
         ds = self.drop_shadow
         return ds.get_height(), ds.get_height()
 
-    def on_draw(self, widget, cr, drop_shadow, icon, ds_xo, ds_yo, normal_rgb, active_rgb, xo=0, yo=0):
+    def on_draw(self, widget, cr, drop_shadow, icon, ds_xo, ds_yo, xo=0, yo=0):
         a = widget.get_allocation()
 
         # dropshadow
         x = (a.width - drop_shadow.get_width()) * 0.5 + ds_xo + xo
         y = (a.height - drop_shadow.get_height()) * 0.5 + ds_yo + yo
         cr.set_source_surface(drop_shadow, x, y)
-        cr.paint()
+        cr.paint_with_alpha(0.75)
 
         # colorised icon
         state = widget.get_state_flags()
-        if state == Gtk.StateFlags.PRELIGHT or state == Gtk.StateFlags.ACTIVE:
-            Gdk.cairo_set_source_rgba(cr, active_rgb)
-        else:
-            Gdk.cairo_set_source_rgba(cr, normal_rgb)
+        context = widget.get_style_context()
+        color = context.get_color(state)
+        Gdk.cairo_set_source_rgba(cr, color)
         x = (a.width - icon.get_width()) * 0.5 + xo
         y = (a.height - icon.get_height()) * 0.5 + yo
         cr.mask_surface(icon, x, y)
@@ -76,13 +72,19 @@ class RotationAboutCenterAnimation(object):
     def __init__(self):
         self.rotation = 0
         self.animator = None
+        self._stop_requested = False
 
     def new_frame(self):
+        _continue = True
         self.rotation += self.ROTATION_INCREMENT
         if self.rotation >= _2PI:
             self.rotation = 0
+            if self._stop_requested:
+                self.animator = None
+                self._stop_requested = False
+                _continue = False
         self.queue_draw()
-        return True
+        return _continue
 
     def start(self):
         if self.is_animating(): return
@@ -92,9 +94,7 @@ class RotationAboutCenterAnimation(object):
 
     def stop(self):
         if not self.is_animating(): return
-        GObject.source_remove(self.animator)
-        self.rotation = 0
-        self.queue_draw()
+        self._stop_requested = True
         return
 
     def is_animating(self):
@@ -103,32 +103,78 @@ class RotationAboutCenterAnimation(object):
 
 class PendingSymbolicIcon(SymbolicIcon, RotationAboutCenterAnimation):
 
+    BUBBLE_BORDER_RADIUS = 5
+    BUBBLE_XPADDING = 4
+    BUBBLE_YPADDING = 0
+    BUBBLE_FONT_DESC = "Bold 8.5"
+
     def __init__(self, name):
         SymbolicIcon.__init__(self, name)
         RotationAboutCenterAnimation.__init__(self)
+
+        # for painting the trans count bubble
+        self.layout = self.create_pango_layout("")
         self.transaction_count = 0
         return
 
     def on_draw(self, widget, cr, *args, **kwargs):
+        cr.save()
         if self.is_animating():
+            # translate to the center, then set the rotation
             a = widget.get_allocation()
             cr.translate(a.width * 0.5, a.height * 0.5)
             cr.rotate(self.rotation)
+            # pass on the translation details
             kwargs['xo'] = -(a.width * 0.5)
             kwargs['yo'] = -(a.height * 0.5)
+
+        # do icon drawing
         SymbolicIcon.on_draw(self, widget, cr, *args, **kwargs)
+        cr.restore()
+
+        if not self.is_animating() or not self.transaction_count: return
+        # paint transactions bubble
+        ex = self.layout.get_pixel_extents()[1]
+        x = (a.width - self.icon.get_width()) / 2 + self.icon.get_width() - ex.width + 2
+        y = (a.height - self.icon.get_height()) / 2 + self.icon.get_height() - ex.height + 2
+        w = ex.width + 2*self.BUBBLE_XPADDING
+        h = ex.height + 2*self.BUBBLE_YPADDING
+
+        rounded_rect(cr, x+1, y+1, w-2, h-2, self.BUBBLE_BORDER_RADIUS)
+        cr.set_source_rgba(0,0,0,0.75)
+        cr.fill()
+
+        rounded_rect(cr, x+0.5, y+0.5, w-1, h-1, self.BUBBLE_BORDER_RADIUS)
+        cr.set_source_rgba(1,1,1, 0.85)
+        cr.set_line_width(1)
+        cr.stroke()
+
+        Gtk.render_layout(widget.get_style_context(), cr,
+                          x + self.BUBBLE_XPADDING,
+                          y + self.BUBBLE_YPADDING,
+                          self.layout)
         return
 
     def set_transaction_count(self, count):
+        if count == self.transaction_count: return
         self.transaction_count = count
+        m = '<span font_desc="%s">%i</span>' % (self.BUBBLE_FONT_DESC,
+                                                count)
+        self.layout.set_markup(m, -1)
+        self.queue_draw()
         return
+
 
 if __name__ == "__main__":
     win = Gtk.Window()
-    icon = "/home/matthew-dev/Projects/usc/lobby-aesthetics/softwarecenter/ui/gtk3/art/icons/available.png"
-    drop_shadow = "/home/matthew-dev/Projects/usc/lobby-aesthetics/softwarecenter/ui/gtk3/art/icons/available-dropshadow.png"
-    ico = SymbolicIcon(icon, drop_shadow)
-    win.add(ico)
+    hb = Gtk.HBox(spacing=12)
+    win.add(hb)
+    ico = SymbolicIcon("available")
+    hb.add(ico)
+    ico = PendingSymbolicIcon("pending")
+    ico.start()
+    ico.set_transaction_count(33)
+    hb.add(ico)
     win.show_all()
     win.connect("destroy", Gtk.main_quit)
     Gtk.main()
