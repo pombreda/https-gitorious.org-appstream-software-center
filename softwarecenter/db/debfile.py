71fd80e1bf99534f76d97b651e837dce88fce154
Gary Lasker <gary.lasker@canonical.com>
2011-07-20 19:51:32 -0400

    better message

--- softwarecenter/db/debfile.py
+++ softwarecenter/db/debfile.py
@@ -92,7 +92,7 @@ class AppDetailsDebFile(AppDetails):
 
         # check deb and set failure state on error
         from softwarecenter.utils import ExecutionTime
-        with ExecutionTime("AppDetailsDebFile._deb.check"):
+        with ExecutionTime("AppDetailsDebFile._deb.check()"):
             if not self._deb.check():
                 self._error = self._deb._failure_string
 
