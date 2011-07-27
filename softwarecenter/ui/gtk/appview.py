c2ddc8ded3a8019fc3fe25b709bd9a6d2cb497f5
Kiwinote <kiwinote@gmail.com>
2011-07-27 14:48:26 +0200

    [ Kiwinote ]   * softwarecenter/ui/gtk/appview.py:     - lowlevel cache isn't available on startup, so query it only on demand     - fix normal cache to correctly filter out unavailable pkgs (LP: #799713)

--- softwarecenter/ui/gtk/appview.py
+++ softwarecenter/ui/gtk/appview.py
@@ -1107,10 +1107,6 @@ class AppViewFilter(xapian.MatchDecider):
         self.distro = get_distro()
         self.db = db
         self.cache = cache
-        try:
-            self.lowlevel_cache = self.cache._cache._cache
-        except:
-            self.lowlevel_cache = None
         self.available_only = False
         self.supported_only = False
         self.installed_only = False
@@ -1156,14 +1152,19 @@ class AppViewFilter(xapian.MatchDecider):
                 not doc.get_value(XapianValues.ARCHIVE_CHANNEL) == AVAILABLE_FOR_PURCHASE_MAGIC_CHANNEL_NAME):
                 return False
         if self.installed_only:
+            try:
+                self.lowlevel_cache = self.cache._cache._cache
+            except:
+                self.lowlevel_cache = None
             # use the lowlevel cache here if available, twice as fast
             if (self.lowlevel_cache and 
                 (not pkgname in self.lowlevel_cache or
                  not self.lowlevel_cache[pkgname].current_ver)):
                 return False
             else:
-                if (pkgname in self.cache and
-                    not self.cache[pkgname].is_installed):
+                if (not pkgname in self.cache or
+                    (pkgname in self.cache and
+                     not self.cache[pkgname].is_installed)):
                     return False
         if self.not_installed_only:
             if (pkgname in self.cache and
