5b715bdb3582c95e106dc1e528e527fa92b0feac
Michael Vogt <michael.vogt@ubuntu.com>
2011-08-10 18:18:04 +0200

    * softwarecenter/ui/gtk3/views/appdetailsview_gtk.py:   - reenable weblive events, thanks to Stephane Graber!

--- softwarecenter/ui/gtk3/views/appdetailsview_gtk.py
+++ softwarecenter/ui/gtk3/views/appdetailsview_gtk.py
@@ -986,11 +986,11 @@ class AppDetailsViewGtk(Gtk.Viewport, AppDetailsViewBase):
         right_vb.pack_start(self.test_drive, False, False, 0)
 
         # attach to all the WebLive events
-        #~ self.weblive.client.connect("progress", self.on_weblive_progress)
-        #~ self.weblive.client.connect("connected", self.on_weblive_connected)
-        #~ self.weblive.client.connect("disconnected", self.on_weblive_disconnected)
-        #~ self.weblive.client.connect("exception", self.on_weblive_exception)
-        #~ self.weblive.client.connect("warning", self.on_weblive_warning)
+        self.weblive.client.connect("progress", self.on_weblive_progress)
+        self.weblive.client.connect("connected", self.on_weblive_connected)
+        self.weblive.client.connect("disconnected", self.on_weblive_disconnected)
+        self.weblive.client.connect("exception", self.on_weblive_exception)
+        self.weblive.client.connect("warning", self.on_weblive_warning)
 
         # homepage link button
         self.homepage_btn = Gtk.Button.new_with_label(_('Developer Web Site'))
