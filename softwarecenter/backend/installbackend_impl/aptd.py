d474d72e9bf215a801be4b0e4f442d428cb558ba
Michael Vogt <michael.vogt@ubuntu.com>
2011-08-19 18:20:57 +0200

    softwarecenter/backend/installbackend_impl/aptd.py, test/test_aptd.py: improve license key writing and add test

--- softwarecenter/backend/installbackend_impl/aptd.py
+++ softwarecenter/backend/installbackend_impl/aptd.py
@@ -455,15 +455,25 @@ class AptdaemonBackend(GObject.GObject, InstallBackend):
         """ add a license key for a purchase """
         self._logger.debug("adding license_key of len: %i to %s" % (
                 len(license_key), license_key_path))
-        if os.access(license_key_path, os.W_OK):
-            if not os.path.exists(license_key_path):
-                f = open(license_key_path, "w")
-                f.write(license_key)
-                f.close()
-        else:
+        # check destination
+        dest = os.path.normpath(license_key_path)
+        if not (dest.startswith("~") or
+                dest.startswith("/opt")):
+            raise Exception("License key file '%s' outside of HOME or /opt" % dest)
+        # check if its inside HOME and if so, just create it
+        if dest.startswith("~"):
+            dest = os.path.expanduser(dest)
+            dirname = os.path.dirname(dest)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            f = open(dest, "w")
+            f.write(license_key)
+            f.close()
+        # check if its in /opt and hand over to apdaemon in this case
+        elif dest.startswith("/opt"):
             try:
                 trans = yield self.aptd_client.add_license_key(
-                    license_key, license_key_path)
+                    license_key, dest)
                 yield self._run_transaction(trans, None, None, None)
             except Exception as e:
                 self._logger.error("add_repository: '%s'" % e)
