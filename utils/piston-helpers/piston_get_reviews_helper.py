0e086279e297d160828029144c5ff5dc80bac1d0
Michael Vogt <michael.vogt@ubuntu.com>
2011-08-19 11:42:18 +0200

    * utils/piston-helpers/piston_get_useful_votes_helper.py,   utils/piston-helpers/piston_get_reviews_helper.py:   - fix exception type now that json is used (instead of simplejson)     LP: #828324

--- utils/piston-helpers/piston_get_reviews_helper.py
+++ utils/piston-helpers/piston_get_reviews_helper.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
         # the backend sometimes returns None so we fix this here
         if piston_reviews is None:
             piston_reviews = []
-    except json.decoder.JSONDecodeError, e:
+    except ValueError as e:
         LOG.error("failed to parse '%s'" % e.doc)
     #bug lp:709408 - don't print 404 errors as traceback when api request 
     #                returns 404 error
