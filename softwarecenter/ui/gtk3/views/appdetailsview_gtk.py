a74e76f968232ee7e152ca0e7b95bde66bc2cb39
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2011-08-23 23:41:28 +1200

    add the ratings count to the bottom of the stars in the detailsview header

--- softwarecenter/ui/gtk3/views/appdetailsview_gtk.py
+++ softwarecenter/ui/gtk3/views/appdetailsview_gtk.py
@@ -55,7 +55,7 @@ from softwarecenter.ui.gtk3.em import StockEms, em
 
 from softwarecenter.ui.gtk3.widgets.reviews import UIReviewsList
 from softwarecenter.ui.gtk3.widgets.containers import SmallBorderRadiusFrame
-from softwarecenter.ui.gtk3.widgets.stars import Star
+from softwarecenter.ui.gtk3.widgets.stars import StarRatingsWidget
 from softwarecenter.ui.gtk3.widgets.description import AppDescription
 from softwarecenter.ui.gtk3.widgets.thumbnail import ScreenshotThumbnail
 from softwarecenter.ui.gtk3.widgets.weblivedialog import (
@@ -935,8 +935,8 @@ class AppDetailsViewGtk(Gtk.Viewport, AppDetailsViewBase):
         #~ vb_inner.pack_start(self.usage, True, True, 0)
 
         # star rating widget
-        self.review_stats_widget = Star()
-        self.review_stats_widget.set_size_as_pixel_value(title_fontsize)
+        self.review_stats_widget = StarRatingsWidget()
+        self.review_stats_widget.stars.set_size_as_pixel_value(title_fontsize)
         #~ a.add(self.review_stats_widget)
         #~ hb.pack_end(a, False, False, 0)
         hb.pack_end(self.review_stats_widget, False, True, 0)
