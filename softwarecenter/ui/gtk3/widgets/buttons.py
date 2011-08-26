b038be0fac181893b23f27c0e6ea1aafd726275e
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2011-08-27 00:53:26 +1200

    make the requested width of FeaturedTiles a global

--- softwarecenter/ui/gtk3/widgets/buttons.py
+++ softwarecenter/ui/gtk3/widgets/buttons.py
@@ -137,9 +137,10 @@ class CategoryTile(TileButton):
         window.set_cursor(None)
         return
 
+
+_global_featured_tile_width = em(11)
 class FeaturedTile(TileButton):
 
-    MAX_WIDTH = em(10)
     INSTALLED_OVERLAY_SIZE = 22
     _MARKUP = '<b><small>%s</small></b>'
 
@@ -201,8 +202,9 @@ class FeaturedTile(TileButton):
                          self.n_ratings.size_request().width +
                          StockEms.MEDIUM * 3
                          )
-        
-            self.MAX_WIDTH = max(self.MAX_WIDTH, req_width)
+            global _global_featured_tile_width
+            _global_featured_tile_width = max(_global_featured_tile_width,
+                                              req_width)
 
         details = AppDetails(db=helper.db, doc=doc)
         price = details.price or _("Free")
@@ -229,7 +231,8 @@ class FeaturedTile(TileButton):
         return
 
     def do_get_preferred_width(self):
-        return self.MAX_WIDTH, self.MAX_WIDTH
+        w = _global_featured_tile_width
+        return w, w
 
     def do_draw(self, cr):
         cr.save()
