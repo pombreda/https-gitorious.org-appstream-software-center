8ad9d1e0b3f54156d213999aadc9f922f375fbab
Gary Lasker <gary.lasker@canonical.com>
2011-09-11 21:44:18 -0400

    we can use the original zoom-in icon, actually

--- softwarecenter/ui/gtk3/widgets/thumbnail.py
+++ softwarecenter/ui/gtk3/widgets/thumbnail.py
@@ -65,7 +65,7 @@ class ScreenshotThumbnail(Gtk.Alignment):
         # zoom cursor
         try:
             theme = Gtk.IconTheme.get_default()
-            zoom_pb = theme.load_icon(Gtk.STOCK_ZOOM_FIT, 22, 0)
+            zoom_pb = self.icons.load_icon("zoom-in", 22, 0)
             # FIXME
             self._zoom_cursor = Gdk.Cursor.new_from_pixbuf(
                                     Gdk.Display.get_default(),
