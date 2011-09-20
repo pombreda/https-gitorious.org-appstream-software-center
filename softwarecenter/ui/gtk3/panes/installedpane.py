406540a49ac5dfbcd6b7647d6821b6d0fdf92a99
Gary Lasker <gary.lasker@canonical.com>
2011-09-20 12:11:31 -0400

    restore a responsiveness tweak when selecting a computer for oneconf that was inadvertently reverted in a recent merge

--- softwarecenter/ui/gtk3/panes/installedpane.py
+++ softwarecenter/ui/gtk3/panes/installedpane.py
@@ -404,7 +404,7 @@ class InstalledPane(SoftwarePane, CategoriesParser):
                           sortmode=SortMethods.BY_ALPHABET,
                           nonapps_visible=self.nonapps_visible,
                           filter=xfilter,
-                          nonblocking_load=False,
+                          nonblocking_load=True,
                           persistent_duplicate_filter=(i>0))
 
             L = len(enq.matches)
