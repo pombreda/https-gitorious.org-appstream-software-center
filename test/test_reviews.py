22cdd1ef0bb2115143cbcbbad29653b4475b1a86
Michael Vogt <michael.vogt@ubuntu.com>
2011-09-23 11:18:21 +0200

    test/test_reviews.py: add review-stats testcase

---
+++ test/test_reviews.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import tempfile
+import unittest
+
+import sys
+sys.path.insert(0,"../")
+
+from gi.repository import GObject
+
+import softwarecenter.paths
+softwarecenter.paths.SOFTWARE_CENTER_CACHE_DIR = tempfile.mkdtemp()
+
+from softwarecenter.backend.reviews import (
+    ReviewLoaderSpawningRNRClient as ReviewLoader)
+from softwarecenter.testutils import (get_test_pkg_info, get_test_db)
+
+class TestReviewLoader(unittest.TestCase):
+
+    def setUp(self):
+        self.cache = get_test_pkg_info()
+        self.db = get_test_db()
+
+    def _review_stats_ready_callback(self, review_stats):
+        self._stats_ready = True
+        self._review_stats = review_stats
+
+    def test_review_loader(self):
+        self._stats_ready = False
+        self._review_stats = []
+        review_loader = ReviewLoader(self.cache, self.db)
+        review_loader.refresh_review_stats(self._review_stats_ready_callback)
+        while not self._stats_ready:
+            self._p()
+        self.assertTrue(len(self._review_stats) > 0)
+        self.assertTrue(os.path.exists(review_loader.REVIEW_STATS_CACHE_FILE))
+    
+    def _p(self):
+        main_loop = GObject.main_context_default()
+        while main_loop.pending():
+            main_loop.iteration()
+
+if __name__ == "__main__":
+    import logging
+    #logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
