4af11046350fa1dac08db15333d41095d4529487
Michael Vogt <michael.vogt@ubuntu.com>
2011-09-29 20:16:46 +0200

    softwarecenter/ui/gtk3/views/appdetailsview.py: pyflake fix

--- softwarecenter/ui/gtk3/views/appdetailsview.py
+++ softwarecenter/ui/gtk3/views/appdetailsview.py
@@ -185,7 +185,7 @@ class AppDetailsViewBase(object):
         iconname = appdetails.icon
         deb_line = appdetails.deb_line
         license_key = appdetails.license_key
-        license_key = appdetails.license_key_path
+        license_key_path = appdetails.license_key_path
         signing_key_id = appdetails.signing_key_id
         backend = get_install_backend()
         backend.add_repo_add_key_and_install_app(deb_line,
