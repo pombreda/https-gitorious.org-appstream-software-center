68b41d69970cdfb70fd818376ff05c3342d38be1
Michael Vogt <michael.vogt@ubuntu.com>
2011-10-05 11:09:24 +0200

    test/gtk3/test_catview.py: add failing test for #865985

--- test/gtk3/test_catview.py
+++ test/gtk3/test_catview.py
@@ -47,6 +47,43 @@ class TestCatView(unittest.TestCase):
         self.assertEqual(self._cat.name, 'What\xe2\x80\x99s New')
         self.assertEqual(self._cat.sortmode, SortMethods.BY_CATALOGED_TIME)
 
+    def test_subcatview_new_no_sort_info_yet(self):
+        # ensure that we don't show a empty "whats new" category
+        # see LP: #865985
+        from softwarecenter.testutils import get_test_db
+        db = get_test_db()
+        cache = db._aptcache
+        # simulate a fresh install with no catalogedtime info
+        del db._axi_values["catalogedtime"]
+        
+        from softwarecenter.testutils import get_test_gtk3_icon_cache
+        icons = get_test_gtk3_icon_cache()
+
+        from softwarecenter.db.appfilter import AppFilter
+        apps_filter = AppFilter(db, cache)
+
+        from softwarecenter.distro import get_distro
+        import softwarecenter.paths
+        from softwarecenter.paths import APP_INSTALL_PATH
+        from softwarecenter.ui.gtk3.views.catview_gtk import LobbyViewGtk
+        view = LobbyViewGtk(softwarecenter.paths.datadir, APP_INSTALL_PATH,
+                            cache, db, icons, get_distro(), apps_filter)
+        view.show()
+
+        # gui
+        win = Gtk.Window()
+        win.set_size_request(800, 400)
+
+        scroll = Gtk.ScrolledWindow()
+        scroll.add(view)
+        scroll.show()
+        win.add(scroll)
+        win.show()
+        # test visibility
+        self._p()
+        self.assertFalse(view.new_frame.get_property("visible"))
+        self._p()
+
     def _p(self):
         for i in range(5):
             time.sleep(0.1)
