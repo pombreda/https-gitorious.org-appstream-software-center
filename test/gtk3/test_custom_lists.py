b9828a546e61bf8d193357269b7a6c77a9ea8e63
Gary Lasker <gary.lasker@canonical.com>
2011-11-14 19:28:04 -0500

    revamp test for the new design, make sure proper number of items are returned...still more to check

--- test/gtk3/test_custom_lists.py
+++ test/gtk3/test_custom_lists.py
@@ -1,75 +1,46 @@
 #!/usr/bin/python
 
-import apt
-import gtk
-import logging
+from gi.repository import Gtk, GObject
 import sys
 import time
 import unittest
 
-from mock import Mock
+sys.path.insert(0,"../..")
+sys.path.insert(0,"..")
 
-sys.path.insert(0, "..")
+#from mock import Mock
 
-from softwarecenter.ui.gtk.app import SoftwareCenterApp
-from softwarecenter.paths import XAPIAN_BASE_PATH
-from softwarecenter.ui.gtk.appview import AppStore
+TIMEOUT=300
 
-# see https://wiki.ubuntu.com/SoftwareCenter/SearchTesting
+import softwarecenter.paths
+softwarecenter.paths.datadir = "../data"
 
-class TestCustomLists(unittest.TestCase):
-    
-    def _p(self):
-        while gtk.events_pending():
-            gtk.main_iteration()
-
-    def setUp(self):
-        # options
-        mock_options = Mock()
-        mock_options.enable_lp = False
-        mock_options.enable_buy = True
-        apt.apt_pkg.config.set("Dir::log::history", "/tmp")
-        apt.apt_pkg.config.set("Dir::state::lists", "/tmp")
-        self.app = SoftwareCenterApp("../data", XAPIAN_BASE_PATH, mock_options)
-        self.app.window_main.show_all()
-        print "foo0"
-        self._p()
-        print "foo1"
-
-    def _run_search(self, search_text):
-        logging.info("_run_search", search_text)
-        self.app.available_pane.searchentry.delete_text(0, -1)
-        self.app.available_pane.searchentry.insert_text(search_text)
-        for i in range(5):
-            self._p()
-            time.sleep(0.5)
-        return self.app.available_pane.app_view.get_model()
-        
-    def _debug(self, index, model, needle):
-        print "Expected '%s' at index '%s', and custom list contained: '%s'" % (needle, index, model[index][AppStore.COL_PKGNAME])
-
-    def assertPkgInListAtIndex(self, index, model, needle):
-        self.assertEqual(model[index][AppStore.COL_PKGNAME], 
-                         needle, self._debug(index, model, needle))
+class TestSearch(unittest.TestCase):
 
     def test_custom_lists(self):
-        # assert we generate the correct custom list
-        model = self._run_search("ark,artha,software-center")
-        self.assertPkgInListAtIndex(0, model, "ark")
-        self.assertPkgInListAtIndex(1, model, "artha")
-        self.assertPkgInListAtIndex(2, model, "software-center")
+        from softwarecenter.ui.gtk3.panes.availablepane import get_test_window
+        win = get_test_window()
+        pane = win.get_data("pane")
+        self._p()
+        pane.on_search_terms_changed(None, "ark,artha,software-center")
+        self._p()
+        model = pane.app_view.tree_view.get_model()
+        # custom list should return three items
+        self.assertTrue(len(model) == 3)
+        GObject.timeout_add(TIMEOUT, lambda: win.destroy())
+        Gtk.main()
         
-        # check that the "Custom List" button appears in the navigation bar
-        self.assertEqual(
-            str(self.app.available_pane.navigation_bar.get_parts()),
-            "[Get Software, Custom List]")
-        
-        # check that the status bar indicates the correct number of packages
-        # in the list
-        status_bar_text = self.app.label_status.get_text()
-        self.assertTrue(status_bar_text.startswith("3"))
-        
-        # TODO: test action bar functionality
+
+
+    def _p(self):
+        for i in range(10):
+            time.sleep(0.1)
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+
 
 if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
     unittest.main()
+
