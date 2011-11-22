3db0c803b499cd4289f9cd9472b011d48702d81d
Michael Vogt <michael.vogt@ubuntu.com>
2011-11-22 18:26:23 +0100

    releasing version 5.0.3

--- test/test_purchase_backend.py
+++ test/test_purchase_backend.py
@@ -51,8 +51,8 @@ class TestPurchaseBackend(unittest.TestCase):
 		while Gtk.events_pending():
 			Gtk.main_iteration()
 		time.sleep(0.1)
-        self.assertTrue(os.path.exists(self.LICENSE_KEY_PATH))
-        self.assertEqual(open(self.LICENSE_KEY_PATH).read(), self.LICENSE_KEY)
+            self.assertTrue(os.path.exists(self.LICENSE_KEY_PATH))
+            self.assertEqual(open(self.LICENSE_KEY_PATH).read(), self.LICENSE_KEY)
         #time.sleep(10)
         
     def _add_pw_to_commercial_repo(self):
