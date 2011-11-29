39c1d05efdeaaa8d149c57d687f5c9ce3d91cea6
Gary Lasker <gary.lasker@canonical.com>
2011-11-29 16:54:41 -0500

    update unit test for the latest design

--- test/gtk3/test_unity_launcher_integration.py
+++ test/gtk3/test_unity_launcher_integration.py
@@ -54,76 +54,10 @@ class TestUnityLauncherIntegration(unittest.TestCase):
                                     TransactionTypes.INSTALL)
         # wait a wee bit
         self._zzz()
-        
-    def test_unity_launcher_stays_after_install_finished(self):
-        # we are testing the prompted add to launcher enabled functionality,
-        # so shut off the automatic adds
-        available_pane.add_to_launcher_enabled = False
-        test_pkgname = "gl-117"
-        mock_result = Mock()
-        mock_result.pkgname = test_pkgname
-        mock_result.success = True
-        # now pretend
-        # now pretend
-        self._navigate_to_appdetails_and_install(test_pkgname)
-        # pretend we are done
-        available_pane.backend.emit("transaction-finished", mock_result)
-        # this is normally set in the transaction-finished call but our
-        # app is not really installed so we need to mock it here
-        available_pane.unity_launcher.launcher_queue[test_pkgname].installed_desktop_file_path = "/some/path"
-        # wait a wee bit
-        self._zzz()
-        # ensure we still have the button
-        button = available_pane.action_bar.get_button(
-                ActionButtons.ADD_TO_LAUNCHER)
-        self.assertNotEqual(button, None)
-        self.assertTrue(button.get_property("visible"))
-        # now click it even though the transaction is over
-        button.clicked()
-        self._zzz()
-        # ensure the add to launcher button is now hidden
-        button = available_pane.action_bar.get_button(
-                ActionButtons.ADD_TO_LAUNCHER)
-        self.assertEqual(button, None)
 
     def test_unity_launcher_integration(self):
         # we are testing the prompted add to launcher enabled functionality,
         # so shut off the automatic adds
-        available_pane.add_to_launcher_enabled = False
-        test_pkgname = "lincity-ng"
-        mock_result = Mock()
-        mock_result.pkgname = test_pkgname
-        mock_result.success = True
-        # now pretend
-        self._navigate_to_appdetails_and_install(test_pkgname)
-        
-        # verify that the panel is shown offering to add the app to the launcher
-        self.assertTrue(available_pane.action_bar.get_property("visible"))
-        button = available_pane.action_bar.get_button(
-            ActionButtons.ADD_TO_LAUNCHER)
-        self.assertTrue(button is not None)
-        # click the button 
-        button.clicked()
-
-        # check that a correct UnityLauncherInfo object has been created and
-        # added to the queue
-        self.assertTrue(test_pkgname in available_pane.unity_launcher.launcher_queue)
-        launcher_info = available_pane.unity_launcher.remove_from_launcher_queue(test_pkgname)
-        # check the UnityLauncherInfo values themselves
-        self.assertEqual(launcher_info.name, "lincity-ng")
-        self.assertEqual(launcher_info.icon_name, "lincity-ng")
-        self.assertTrue(launcher_info.icon_x > 5)
-        self.assertTrue(launcher_info.icon_y > 5)
-        self.assertEqual(launcher_info.icon_size, 96)
-        self.assertEqual(launcher_info.app_install_desktop_file_path,
-            "/usr/share/app-install/desktop/lincity-ng:lincity-ng.desktop")
-        self.assertEqual(launcher_info.trans_id, "testid101")
-        # finally, make sure the the app has been removed from the launcher
-        # queue        
-        self.assertFalse(test_pkgname in available_pane.unity_launcher.launcher_queue)
-        
-    def test_automatic_unity_launcher_integration(self):
-        # we are testing the automatic add to launcher enabled functionality
         available_pane.add_to_launcher_enabled = True
         test_pkgname = "lincity-ng"
         mock_result = Mock()
@@ -132,11 +66,6 @@ class TestUnityLauncherIntegration(unittest.TestCase):
         # now pretend
         self._navigate_to_appdetails_and_install(test_pkgname)
         
-        # verify that the panel is *not* shown offering to add the app to the launcher
-        button = available_pane.action_bar.get_button(
-            ActionButtons.ADD_TO_LAUNCHER)
-        self.assertTrue(button is None)
-
         # check that a correct UnityLauncherInfo object has been created and
         # added to the queue
         self.assertTrue(test_pkgname in available_pane.unity_launcher.launcher_queue)
@@ -153,7 +82,7 @@ class TestUnityLauncherIntegration(unittest.TestCase):
         # finally, make sure the the app has been removed from the launcher
         # queue        
         self.assertFalse(test_pkgname in available_pane.unity_launcher.launcher_queue)
-        
+
     def test_desktop_file_path_conversion(self):
         # test 'normal' case
         app_install_desktop_path = ("./data/app-install/desktop/" +
