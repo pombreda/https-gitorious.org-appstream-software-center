20e3ab2339482a101ab688484fb1e85667f326e8
Brendan Donegan <brendan.donegan@canonical.com>
2011-12-02 11:59:39 +0000

    Added a bunch of tests to test_debfileapplication.py

--- test/test_debfileapplication.py
+++ test/test_debfileapplication.py
@@ -3,15 +3,51 @@
 import os
 import sys
 import unittest
+import logging
 
 sys.path.insert(0, '../')
 
-from softwarecenter.db.debfile import DebFileApplication
 
-DEBFILE_PATH = '/home/brendand/checkbox_0.13.1+bzr1129+201111241945~oneiric1_i386.deb'
+from softwarecenter.enums import PkgStates
+from softwarecenter.db.debfile import DebFileApplication
+from softwarecenter.testutils import get_test_db
+
+DEBFILE_PATH = '/home/brendand/Downloads/terminator_0.96ppa6_all.deb'
+DEBFILE_UNINSTALLED = '/home/brendand/Downloads/shutter_0.88~ppa4~oneiric1_all.deb'
+
+DEBFILE_NAME = 'terminator'
+DEBFILE_DESCRIPTION = ' Terminator is a little project to produce an efficient way of\n filling a large area of screen space with terminals.\n The user can have multiple terminals in one window and use\n key bindings to switch between them. See the manpage for\n details.'
 
 class TestDebFileApplication(unittest.TestCase):
     """ Test the class DebFileApplication """
 
-    def setup(self):
-        self.debfileapplication = DebFileApplication(DEBFILE_PATH)
+    def setUp(self):
+        self.db = get_test_db()
+
+    def test_get_name(self):
+        debfileapplication = DebFileApplication(DEBFILE_PATH)
+        debfiledetails = debfileapplication.get_details(self.db)
+        
+        self.assertEquals(debfiledetails.name, DEBFILE_NAME)
+
+    def test_get_description(self):
+        debfileapplication = DebFileApplication(DEBFILE_PATH)
+        debfiledetails = debfileapplication.get_details(self.db)
+
+        self.assertEquals(debfiledetails.description, DEBFILE_DESCRIPTION)
+
+    def test_get_pkg_state_reinstallable(self):
+        debfileapplication = DebFileApplication(DEBFILE_PATH)
+        debfiledetails = debfileapplication.get_details(self.db)
+
+        self.assertEquals(debfiledetails.pkg_state, PkgStates.REINSTALLABLE)
+
+    def test_get_pkg_state_uninstalled(self):
+        debfileapplication = DebFileApplication(DEBFILE_UNINSTALLED)
+        debfiledetails = debfileapplication.get_details(self.db)
+
+        self.assertEquals(debfiledetails.pkg_state, PkgStates.UNINSTALLED)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
