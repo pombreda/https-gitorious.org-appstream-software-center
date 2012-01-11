40021501468027966c9fc0fac4078529a308d5e7
Gary Lasker <gary.lasker@canonical.com>
2012-01-11 17:59:01 -0500

    make it work for the list view also (still using the current 4.0 approach that doesn't refresh automatically)

--- softwarecenter/models/appstore.py
+++ softwarecenter/models/appstore.py
@@ -381,7 +381,7 @@ class AppStore(gtk.GenericTreeModel):
         if result.pkgname in self.transaction_index_map:
             del self.transaction_index_map[result.pkgname]
 
-    def _download_icon_and_show_when_ready(self, cache, pkgname, icon_file_name):
+    def _download_icon_and_show_when_ready(self, cache, pkgname, icon_file_name, icon_url):
         self._logger.debug("did not find the icon locally, must download %s" % icon_file_name)
         def on_image_download_complete(downloader, image_file_path):
             pb = gtk.gdk.pixbuf_new_from_file_at_size(icon_file_path,
@@ -391,7 +391,13 @@ class AppStore(gtk.GenericTreeModel):
             icon_file = os.path.splitext(os.path.basename(image_file_path))[0]
             self.icon_cache[icon_file] = pb
 
-        url = get_distro().get_downloadable_icon_url(cache, pkgname, icon_file_name)
+        if icon_url:
+            url = icon_url
+        else:
+            # if the icon url has not been provided by the agent, then this
+            # downloadable icon is from the extras repository and so we must
+            # generate the download url
+            url = get_distro().get_downloadable_icon_url(cache, pkgname, icon_file_name)
         if url is not None:
             icon_file_path = os.path.join(SOFTWARE_CENTER_ICON_CACHE_DIR, icon_file_name)
             image_downloader = SimpleFileDownloader()
@@ -517,11 +523,10 @@ class AppStore(gtk.GenericTreeModel):
                         # check if this is a downloadable icon for an
                         # application provided by the agent
                         icon_url = self.db.get_icon_url(doc)
-                        if icon_url:
-                            print ">>> icon_url: ", icon_url
                         self._download_icon_and_show_when_ready(self.cache, 
                                                                 app.pkgname,
-                                                                icon_file_name)
+                                                                icon_file_name,
+                                                                icon_url)
                         # display the missing icon while the real one downloads
                         self.icon_cache[icon_name] = self._appicon_missing_icon
             except glib.GError, e:
