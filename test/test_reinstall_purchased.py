8fd948ab0e651f86f0fd0e15dedc03d9f62f0664
Michael Nelson <michael.nelson@canonical.com>
2012-01-17 12:59:02 +0100

    REFACTOR: Added more real data for tests.

--- test/test_reinstall_purchased.py
+++ test/test_reinstall_purchased.py
@@ -19,27 +19,74 @@ from softwarecenter.db.update import (
     SoftwareCenterAgentParser,
     )
 
-# The original lucid API (1.0) as documented at:
-#  https://wiki.canonical.com/Ubuntu/SoftwareCenter/10.10/Roadmap/SoftwareCenterAgent
-# is no longer used by USC. The example JSON below should be taken from running
-# USC against production.
-AVAILABLE_FOR_ME_JSON = """
+# Example taken from running:
+# PYTHONPATH=. utils/piston-helpers/piston_generic_helper.py --output=pickle \
+#           --debug --needs-auth SoftwareCenterAgentAPI subscriptions_for_me
+# then:
+#    f = open('my_subscriptions.pickle')
+#    subscriptions = pickle.load(f)
+#    completed_subs = [subs for subs in subscriptions if subs.state=='Complete']
+#    completed_subs[0].__dict__
+SUBSCRIPTIONS_FOR_ME_JSON = """
 [
     {
-        "deb_line": "deb https://username:randomp3atoken@private-ppa.launchpad.net/mvo/private-test/ubuntu maverick main #Personal access of username to private-test",
-        "purchase_price": "19.95",
-        "purchase_date": "2010-06-24 20:08:23",
-        "state": "Complete",
-        "failures": [],
-        "open_id": "https://login.ubuntu.com/+id/ABCDEF",
-        "application": {
-            "archive_id": "mvo/private-test",
-            "signing_key_id": "1024R/0EB12F05",
-            "name": "Ubiteme",
-            "package_name": "hellox",
-            "description": "One of the best strategy games you\'ll ever play!"
-            },
-        "distro_series": {"version": "10.10", "code_name": "maverick"}
+         "deb_line": "deb https://username:random3atoken@private-ppa.launchpad.net/commercial-ppa-uploaders/photobomb/ubuntu natty main",
+         "purchase_price": "2.99",
+         "purchase_date": "2011-09-16 06:37:52",
+         "state": "Complete",
+         "failures": [],
+         "open_id": "https://login.ubuntu.com/+id/ABCDEF",
+         "application": {
+              "archive_id": "commercial-ppa-uploaders/photobomb",
+              "signing_key_id": "1024R/75254D99",
+              "name": "Photobomb",
+              "package_name": "photobomb",
+              "description": "Easy and Social Image Editor\\nPhotobomb give you easy access to images in your social networking feeds, pictures on your computer and peripherals, and pictures on the web, and let\'s you draw, write, crop, combine, and generally have a blast mashing \'em all up. Then you can save off your photobomb, or tweet your creation right back to your social network."
+         },
+         "distro_series": {"code_name": "natty", "version": "11.04"}
+    }
+]
+"""
+# Taken directly from:
+# https://software-center.ubuntu.com/api/2.0/applications/en/ubuntu/oneiric/i386/
+AVAILABLE_APPS_JSON = """
+[
+    {
+        "archive_id": "commercial-ppa-uploaders/fluendo-dvd",
+        "signing_key_id": "1024R/75254D99",
+        "license": "Proprietary",
+        "name": "Fluendo DVD Player",
+        "package_name": "fluendo-dvd",
+        "support_url": "",
+        "series": {
+            "maverick": [
+                "i386",
+                "amd64"
+            ],
+            "natty": [
+                "i386",
+                "amd64"
+            ],
+            "oneiric": [
+                "i386",
+                "amd64"
+            ]
+        },
+        "price": "24.95",
+        "demo": null,
+        "date_published": "2011-12-05 18:43:21.653868",
+        "status": "Published",
+        "channel": "For Purchase",
+        "icon_data": "...",
+        "department": [
+            "Sound & Video"
+        ],
+        "archive_root": "https://private-ppa.launchpad.net/",
+        "screenshot_url": "http://software-center.ubuntu.com/site_media/screenshots/2011/05/fluendo-dvd-maverick_.png",
+        "tos_url": "https://software-center.ubuntu.com/licenses/3/",
+        "icon_url": "http://software-center.ubuntu.com/site_media/icons/2011/05/fluendo-dvd.png",
+        "categories": "AudioVideo",
+        "description": "Play DVD-Videos\r\n\r\nFluendo DVD Player is a software application specially designed to\r\nreproduce DVD on Linux/Unix platforms, which provides end users with\r\nhigh quality standards.\r\n\r\nThe following features are provided:\r\n* Full DVD Playback\r\n* DVD Menu support\r\n* Fullscreen support\r\n* Dolby Digital pass-through\r\n* Dolby Digital 5.1 output and stereo downmixing support\r\n* Resume from last position support\r\n* Subtitle support\r\n* Audio selection support\r\n* Multiple Angles support\r\n* Support for encrypted discs\r\n* Multiregion, works in all regions\r\n* Multiple video deinterlacing algorithms"
     }
 ]
 """
@@ -54,7 +101,7 @@ class MockAvailableForMeItem(object):
 class MockAvailableForMeList(list):
 
     def __init__(self):
-        alist = json.loads(AVAILABLE_FOR_ME_JSON)
+        alist = json.loads(SUBSCRIPTIONS_FOR_ME_JSON)
         for entry_dict in alist:
             self.append(MockAvailableForMeItem(entry_dict))
 
@@ -74,7 +121,7 @@ class TestPurchased(unittest.TestCase):
         # test if the mocks are ok
         self.assertEqual(len(self.available_to_me), 1)
         self.assertEqual(
-            self.available_to_me[0].application['package_name'], "hellox")
+            self.available_to_me[0].application['package_name'], "photobomb")
 
     def test_reinstall_purchased_xapian(self):
         db = StoreDatabase("/var/cache/software-center/xapian", self.cache)
@@ -93,18 +140,20 @@ class TestPurchased(unittest.TestCase):
         self.assertEqual(len(matches), 1)
         for m in matches:
             doc = db.xapiandb.get_document(m.docid)
-            self.assertEqual(doc.get_value(XapianValues.PKGNAME), "hellox")
-            self.assertEqual(doc.get_value(XapianValues.ARCHIVE_SIGNING_KEY_ID), "1024R/0EB12F05")
+            self.assertEqual(doc.get_value(XapianValues.PKGNAME), "photobomb")
+            self.assertEqual(
+                doc.get_value(XapianValues.ARCHIVE_SIGNING_KEY_ID),
+                "1024R/75254D99")
             self.assertEqual(doc.get_value(XapianValues.ARCHIVE_DEB_LINE),
-                                           "deb https://username:randomp3atoken@private-ppa.launchpad.net/mvo/private-test/ubuntu maverick main #Personal access of username to private-test")
-            break # only one match
+                "deb https://username:random3atoken@"
+                 "private-ppa.launchpad.net/commercial-ppa-uploaders"
+                 "/photobomb/ubuntu natty main")
 
 
 class SoftwareCenterAgentParserTestCase(unittest.TestCase):
 
-    # def test_parses_application(self):
-    #     self.fail("Unimplemented")
-    pass
+    def test_parses_application_from_available_apps(self):
+        pass
 
 
 class SCAPurchasedApplicationParserTestCase(unittest.TestCase):
@@ -112,7 +161,7 @@ class SCAPurchasedApplicationParserTestCase(unittest.TestCase):
     def _make_application_parser(self, piston_subscription=None):
         if piston_subscription is None:
             piston_subscription = PistonResponseObject.from_dict(
-                json.loads(AVAILABLE_FOR_ME_JSON)[0])
+                json.loads(SUBSCRIPTIONS_FOR_ME_JSON)[0])
 
         return SCAPurchasedApplicationParser(piston_subscription)
 
@@ -120,11 +169,10 @@ class SCAPurchasedApplicationParserTestCase(unittest.TestCase):
         parser = self._make_application_parser()
 
         expected_results = {
-            "Deb-Line": "deb https://username:randomp3atoken@"
-                        "private-ppa.launchpad.net/mvo/private-test/ubuntu "
-                        "maverick main #Personal access of username to "
-                        "private-test",
-            "Purchased-Date": "2010-06-24 20:08:23",
+             "Deb-Line": "deb https://username:random3atoken@"
+                         "private-ppa.launchpad.net/commercial-ppa-uploaders"
+                         "/photobomb/ubuntu natty main",
+             "Purchased-Date": "2011-09-16 06:37:52",
             }
         for key in expected_results:
             result = parser.get_desktop(key)
@@ -136,10 +184,10 @@ class SCAPurchasedApplicationParserTestCase(unittest.TestCase):
         parser = self._make_application_parser()
 
         expected_results = {
-            "Name": "Ubiteme (already purchased)",
-            "Package": "hellox",
-            "Signing-Key-Id": "1024R/0EB12F05",
-            "PPA": "mvo/private-test",
+            "Name": "Photobomb (already purchased)",
+            "Package": "photobomb",
+            "Signing-Key-Id": "1024R/75254D99",
+            "PPA": "commercial-ppa-uploaders/photobomb",
             }
         for key in expected_results.keys():
             result = parser.get_desktop(key)
@@ -158,7 +206,7 @@ class SCAPurchasedApplicationParserTestCase(unittest.TestCase):
 
     def test_license_key_present(self):
         piston_subscription = PistonResponseObject.from_dict(
-            json.loads(AVAILABLE_FOR_ME_JSON)[0])
+            json.loads(SUBSCRIPTIONS_FOR_ME_JSON)[0])
         piston_subscription.license_key = 'abcd'
         piston_subscription.license_key_path = '/foo'
         parser = self._make_application_parser(piston_subscription)
