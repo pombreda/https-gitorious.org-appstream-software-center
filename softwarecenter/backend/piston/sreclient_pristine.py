bf9c75bacf2f9e69c11e3a41558c44c6d2b329b4
Gary Lasker <gary.lasker@canonical.com>
2012-02-14 10:59:13 -0500

    REFACTOR: import the latest sreclient from lp:~canonical-ca-hackers/ubuntu-recommender/ubuntu-recommender-client, thanks achuni

--- softwarecenter/backend/piston/sreclient_pristine.py
+++ softwarecenter/backend/piston/sreclient_pristine.py
@@ -1,5 +1,6 @@
 from piston_mini_client import (PistonAPI, returns_json)
 from piston_mini_client.validators import (
+    validate,
     validate_integer,
     validate_pattern,
     oauth_protected,
@@ -15,18 +16,18 @@ class SoftwareCenterRecommenderAPI(PistonAPI):
 
     @returns_json
     def server_status(self):
-        return self._get('server-status', scheme=PUBLIC_API_SCHEME)
+        return self._get('server-status/', scheme=PUBLIC_API_SCHEME)
 
     @oauth_protected
     @returns_json
     def profile(self, pkgnames):
         """Return True if a profile has already been uploaded."""
-        return self._get('profile', scheme=AUTHENTICATED_API_SCHEME)
+        return self._get('profile/', scheme=AUTHENTICATED_API_SCHEME)
 
     @oauth_protected
     @returns_json
     def submit_profile(self, data):
-        return self._post('profile', data=data,
+        return self._post('profile/', data=data,
             scheme=AUTHENTICATED_API_SCHEME)
 
     @returns_json
@@ -40,7 +41,7 @@ class SoftwareCenterRecommenderAPI(PistonAPI):
 
     @oauth_protected
     def recommend_me(self):
-        return self._get('recommend_me', scheme=AUTHENTICATED_API_SCHEME)
+        return self._get('recommend_me/', scheme=AUTHENTICATED_API_SCHEME)
 
     @validate_pattern('pkgname', '[^/]+')
     @returns_json
@@ -54,7 +55,7 @@ class SoftwareCenterRecommenderAPI(PistonAPI):
 
     @returns_json
     def recommend_top(self):
-        return self._get('recommend_top', scheme=PUBLIC_API_SCHEME)
+        return self._get('recommend_top/', scheme=PUBLIC_API_SCHEME)
 
     @oauth_protected
     @validate_pattern('rid', '\w+')
@@ -65,5 +66,16 @@ class SoftwareCenterRecommenderAPI(PistonAPI):
             'feedback': feedback,
             'rid': rid,
             }
-        return self._post('feedback', data=data,
+        return self._post('feedback/', data=data,
+            scheme=AUTHENTICATED_API_SCHEME)
+
+    @oauth_protected
+    @returns_json
+    @validate('remove', bool)
+    def remove_app(self, appname, remove):
+        data = {
+            'app': appname,
+            'remove': remove,
+            }
+        return self._post('remove_app/', data=data,
             scheme=AUTHENTICATED_API_SCHEME)
