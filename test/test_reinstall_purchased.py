59eb3a569879eafde6b87b9c29b54767d1c17c7d
Michael Vogt <michael.vogt@ubuntu.com>
2012-02-15 19:02:04 +0100

    test/test_reinstall_purchased.py: remove "tags" as its no longer needed, its debtags on the agent now

--- test/test_reinstall_purchased.py
+++ test/test_reinstall_purchased.py
@@ -163,7 +163,6 @@ class SCAApplicationParserTestCase(unittest.TestCase):
 
         # Delete the keys which are not yet provided via the API:
         del(inverse_map['video_embedded_html_url'])
-        del(inverse_map['tags'])
 
         for key in inverse_map:
             self.assertTrue(parser.has_option_desktop(inverse_map[key]),
