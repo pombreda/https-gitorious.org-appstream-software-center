30cdd7a862cfd3aeebc31bda031f453207227333
Gary Lasker <gary.lasker@canonical.com>
2012-02-16 13:23:59 -0500

    REFACTOR: just uncomment out catview tests that were temporarily commented out but inadvertently left in in last checkin

--- test/gtk3/test_catview.py
+++ test/gtk3/test_catview.py
@@ -19,161 +19,161 @@ class TestCatView(unittest.TestCase):
         #print "**************", subcatview, category
         self._cat = category
     
-#    def test_subcatview_top_rated(self):
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        lobby = win.get_data("lobby")
-#        # test clicking top_rated
-#        lobby.connect("category-selected", self._on_category_selected)
-#        lobby.top_rated_frame.more.clicked()
-#        self._p()
-#        self.assertNotEqual(self._cat, None)
-#        self.assertEqual(self._cat.name, "Top Rated")
-#        self.assertEqual(self._cat.sortmode, SortMethods.BY_TOP_RATED)
-#        win.destroy()
+    def test_subcatview_top_rated(self):
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        lobby = win.get_data("lobby")
+        # test clicking top_rated
+        lobby.connect("category-selected", self._on_category_selected)
+        lobby.top_rated_frame.more.clicked()
+        self._p()
+        self.assertNotEqual(self._cat, None)
+        self.assertEqual(self._cat.name, "Top Rated")
+        self.assertEqual(self._cat.sortmode, SortMethods.BY_TOP_RATED)
+        win.destroy()
 
-#    def test_subcatview_new(self):
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        lobby = win.get_data("lobby")
-#        # test clicking new
-#        lobby.connect("category-selected", self._on_category_selected)
-#        lobby.whats_new_frame.more.clicked()
-#        self._p()
-#        self.assertNotEqual(self._cat, None)
-#        # encoding is utf-8 (since r2218, see category.py)
-#        self.assertEqual(self._cat.name, 'What\xe2\x80\x99s New')
-#        self.assertEqual(self._cat.sortmode, SortMethods.BY_CATALOGED_TIME)
-#        win.destroy()
+    def test_subcatview_new(self):
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        lobby = win.get_data("lobby")
+        # test clicking new
+        lobby.connect("category-selected", self._on_category_selected)
+        lobby.whats_new_frame.more.clicked()
+        self._p()
+        self.assertNotEqual(self._cat, None)
+        # encoding is utf-8 (since r2218, see category.py)
+        self.assertEqual(self._cat.name, 'What\xe2\x80\x99s New')
+        self.assertEqual(self._cat.sortmode, SortMethods.BY_CATALOGED_TIME)
+        win.destroy()
 
-#    def test_subcatview_new_no_sort_info_yet(self):
-#        # ensure that we don't show a empty "whats new" category
-#        # see LP: #865985
-#        from softwarecenter.testutils import get_test_db
-#        db = get_test_db()
-#        cache = db._aptcache
-#        # simulate a fresh install with no catalogedtime info
-#        del db._axi_values["catalogedtime"]
-#        
-#        from softwarecenter.testutils import get_test_gtk3_icon_cache
-#        icons = get_test_gtk3_icon_cache()
+    def test_subcatview_new_no_sort_info_yet(self):
+        # ensure that we don't show a empty "whats new" category
+        # see LP: #865985
+        from softwarecenter.testutils import get_test_db
+        db = get_test_db()
+        cache = db._aptcache
+        # simulate a fresh install with no catalogedtime info
+        del db._axi_values["catalogedtime"]
+        
+        from softwarecenter.testutils import get_test_gtk3_icon_cache
+        icons = get_test_gtk3_icon_cache()
 
-#        from softwarecenter.db.appfilter import AppFilter
-#        apps_filter = AppFilter(db, cache)
+        from softwarecenter.db.appfilter import AppFilter
+        apps_filter = AppFilter(db, cache)
 
-#        from softwarecenter.distro import get_distro
-#        import softwarecenter.paths
-#        from softwarecenter.paths import APP_INSTALL_PATH
-#        from softwarecenter.ui.gtk3.views.catview_gtk import LobbyViewGtk
-#        view = LobbyViewGtk(softwarecenter.paths.datadir, APP_INSTALL_PATH,
-#                            cache, db, icons, get_distro(), apps_filter)
-#        view.show()
+        from softwarecenter.distro import get_distro
+        import softwarecenter.paths
+        from softwarecenter.paths import APP_INSTALL_PATH
+        from softwarecenter.ui.gtk3.views.catview_gtk import LobbyViewGtk
+        view = LobbyViewGtk(softwarecenter.paths.datadir, APP_INSTALL_PATH,
+                            cache, db, icons, get_distro(), apps_filter)
+        view.show()
 
-#        # gui
-#        win = Gtk.Window()
-#        win.set_size_request(800, 400)
+        # gui
+        win = Gtk.Window()
+        win.set_size_request(800, 400)
 
-#        scroll = Gtk.ScrolledWindow()
-#        scroll.add(view)
-#        scroll.show()
-#        win.add(scroll)
-#        win.show()
-#        # test visibility
-#        self._p()
-#        self.assertFalse(view.whats_new_frame.get_property("visible"))
-#        self._p()
-#        win.destroy()
-#       
-#    def test_subcatview_recommended_for_you_opt_in_display(self):
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        lobby = win.get_data("lobby")
-#        rec_panel = lobby.recommended_for_you_panel
-#        self._p()
-#        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
-#        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.CONTENT)
-#        self.assertTrue(rec_panel.opt_in_vbox.get_property("visible"))
-#        win.destroy()
-#        
-#    # patch out the agent query method to avoid making the actual server call
-#    @patch('softwarecenter.backend.recagent.RecommenderAgent'
-#           '.query_submit_profile')
-#    def test_subcatview_recommended_for_you_spinner_display(self, mock_query):
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        lobby = win.get_data("lobby")
-#        rec_panel = lobby.recommended_for_you_panel
-#        self._p()
-#        # click the opt-in button to initiate the process, this will show the spinner
-#        rec_panel.opt_in_button.emit('clicked')
-#        self._p()
-#        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
-#        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.SPINNER)
-#        self.assertTrue(rec_panel.opt_in_vbox.get_property("visible"))
-#        self.assertTrue(rec_panel.spinner.spinner_label)
-#        win.destroy()
-#        
-#    # patch out the agent query method to avoid making the actual server call
-#    @patch('softwarecenter.backend.recagent.RecommenderAgent'
-#           '.query_submit_profile')
-#    def test_subcatview_recommended_for_you_display_recommendations(self, mock_query):
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        lobby = win.get_data("lobby")
-#        rec_panel = lobby.recommended_for_you_panel
-#        self._p()
-#        # click the opt-in button to initiate the process, this will show the spinner
-#        rec_panel.opt_in_button.emit('clicked')
-#        self._p()
-#        rec_panel._update_recommended_for_you_content()
-#        self._p()
-#        # we fake the callback from the agent here
-#        lobby.recommended_for_you_panel.recommended_for_you_cat._recommend_me_result(
-#                                None,
-#                                make_recommender_agent_recommend_me_dict())
-#        self.assertNotEqual(
-#                lobby.recommended_for_you_panel.recommended_for_you_cat.get_documents(self.db), [])
-#        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
-#        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.CONTENT)
-#        self._p()
-#        # test clicking recommended_for_you More button
-#        lobby.connect("category-selected", self._on_category_selected)
-#        lobby.recommended_for_you_panel.more.clicked()
-#        self._p()
-#        self.assertNotEqual(self._cat, None)
-#        self.assertEqual(self._cat.name, "Recommended for You")
-#        win.destroy()
-#        
-#    # patch out the agent query method to avoid making the actual server call
-#    @patch('softwarecenter.backend.recagent.RecommenderAgent'
-#           '.query_recommend_me')
-#    def test_subcatview_recommended_for_you_display_recommendations_not_opted_in(self, mock_query):
-#    
-#        # patch the recommender uuid getter to *not* return a value, this indicates that the user
-#        # has not yet opted in to the recommendations server
-#        get_recommender_uuid_fn = 'softwarecenter.ui.gtk3.widgets.recommendations.RecommendationsPanelCategory.get_recommender_uuid'
-#        get_recommender_uuid_patcher = patch(get_recommender_uuid_fn)
-#        self.addCleanup(get_recommender_uuid_patcher.stop)
-#        mock_get_recommender_uuid = get_recommender_uuid_patcher.start()
-#        mock_get_recommender_uuid.return_value = ""
-#        
-#        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
-#        # get the widgets we need
-#        win = get_test_window_catview()
-#        # we want to work in the "subcat" view
-#        notebook = win.get_child()
-#        notebook.next_page()
-#        
-#        subcat_view = win.get_data("subcat")
-#        rec_cat_panel = subcat_view.recommended_for_you_in_cat
-#        self._p()
-#        self.assertFalse(subcat_view.recommended_for_you_in_cat.get_property("visible"))
-#        win.destroy()
+        scroll = Gtk.ScrolledWindow()
+        scroll.add(view)
+        scroll.show()
+        win.add(scroll)
+        win.show()
+        # test visibility
+        self._p()
+        self.assertFalse(view.whats_new_frame.get_property("visible"))
+        self._p()
+        win.destroy()
+       
+    def test_subcatview_recommended_for_you_opt_in_display(self):
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        lobby = win.get_data("lobby")
+        rec_panel = lobby.recommended_for_you_panel
+        self._p()
+        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
+        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.CONTENT)
+        self.assertTrue(rec_panel.opt_in_vbox.get_property("visible"))
+        win.destroy()
+        
+    # patch out the agent query method to avoid making the actual server call
+    @patch('softwarecenter.backend.recagent.RecommenderAgent'
+           '.query_submit_profile')
+    def test_subcatview_recommended_for_you_spinner_display(self, mock_query):
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        lobby = win.get_data("lobby")
+        rec_panel = lobby.recommended_for_you_panel
+        self._p()
+        # click the opt-in button to initiate the process, this will show the spinner
+        rec_panel.opt_in_button.emit('clicked')
+        self._p()
+        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
+        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.SPINNER)
+        self.assertTrue(rec_panel.opt_in_vbox.get_property("visible"))
+        self.assertTrue(rec_panel.spinner.spinner_label)
+        win.destroy()
+        
+    # patch out the agent query method to avoid making the actual server call
+    @patch('softwarecenter.backend.recagent.RecommenderAgent'
+           '.query_submit_profile')
+    def test_subcatview_recommended_for_you_display_recommendations(self, mock_query):
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        lobby = win.get_data("lobby")
+        rec_panel = lobby.recommended_for_you_panel
+        self._p()
+        # click the opt-in button to initiate the process, this will show the spinner
+        rec_panel.opt_in_button.emit('clicked')
+        self._p()
+        rec_panel._update_recommended_for_you_content()
+        self._p()
+        # we fake the callback from the agent here
+        lobby.recommended_for_you_panel.recommended_for_you_cat._recommend_me_result(
+                                None,
+                                make_recommender_agent_recommend_me_dict())
+        self.assertNotEqual(
+                lobby.recommended_for_you_panel.recommended_for_you_cat.get_documents(self.db), [])
+        from softwarecenter.ui.gtk3.widgets.containers import FramedHeaderBox
+        self.assertTrue(rec_panel.spinner_notebook.get_current_page() == FramedHeaderBox.CONTENT)
+        self._p()
+        # test clicking recommended_for_you More button
+        lobby.connect("category-selected", self._on_category_selected)
+        lobby.recommended_for_you_panel.more.clicked()
+        self._p()
+        self.assertNotEqual(self._cat, None)
+        self.assertEqual(self._cat.name, "Recommended for You")
+        win.destroy()
+        
+    # patch out the agent query method to avoid making the actual server call
+    @patch('softwarecenter.backend.recagent.RecommenderAgent'
+           '.query_recommend_me')
+    def test_subcatview_recommended_for_you_display_recommendations_not_opted_in(self, mock_query):
+    
+        # patch the recommender uuid getter to *not* return a value, this indicates that the user
+        # has not yet opted in to the recommendations server
+        get_recommender_uuid_fn = 'softwarecenter.ui.gtk3.widgets.recommendations.RecommendationsPanelCategory.get_recommender_uuid'
+        get_recommender_uuid_patcher = patch(get_recommender_uuid_fn)
+        self.addCleanup(get_recommender_uuid_patcher.stop)
+        mock_get_recommender_uuid = get_recommender_uuid_patcher.start()
+        mock_get_recommender_uuid.return_value = ""
+        
+        from softwarecenter.ui.gtk3.views.catview_gtk import get_test_window_catview
+        # get the widgets we need
+        win = get_test_window_catview()
+        # we want to work in the "subcat" view
+        notebook = win.get_child()
+        notebook.next_page()
+        
+        subcat_view = win.get_data("subcat")
+        rec_cat_panel = subcat_view.recommended_for_you_in_cat
+        self._p()
+        self.assertFalse(subcat_view.recommended_for_you_in_cat.get_property("visible"))
+        win.destroy()
         
     # patch out the agent query method to avoid making the actual server call
     @patch('softwarecenter.backend.recagent.RecommenderAgent'
