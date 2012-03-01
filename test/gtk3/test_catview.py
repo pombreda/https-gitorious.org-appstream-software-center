463567e806faac746d0b10e8a3be4354df977d15
Michael Vogt <michael.vogt@ubuntu.com>
2012-03-01 12:29:55 +0100

    pyflake fixes

--- test/gtk3/test_catview.py
+++ test/gtk3/test_catview.py
@@ -188,6 +188,6 @@ class TestCatView(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    import logging
+    #import logging
     #logging.basicConfig(level=logging.DEBUG)
     unittest.main()
--- test/gtk3/test_recommendations_widgets.py
+++ test/gtk3/test_recommendations_widgets.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python
 
-from gi.repository import Gtk, GdkPixbuf, GObject
-import os
+from gi.repository import Gtk, GObject
 import unittest
-from gettext import gettext as _
 
-from mock import Mock, patch
-
-from testutils import setup_test_env, do_events
+from testutils import setup_test_env
 setup_test_env()
-from softwarecenter.ui.gtk3.widgets.reviews import get_test_reviews_window
 
-from softwarecenter.ui.gtk3.widgets.labels import (
-    HardwareRequirementsLabel, HardwareRequirementsBox)
-
+from softwarecenter.ui.gtk3.widgets.recommendations import get_test_window
 
 # window destory timeout
 TIMEOUT=100
@@ -24,7 +17,6 @@ TIMEOUT=100
 class TestRecommendationsWidgets(unittest.TestCase):
 
     def test_recommendations_widgets(self):
-        from softwarecenter.ui.gtk3.widgets.recommendations import get_test_window
         win = get_test_window()
         win.show_all()
         GObject.timeout_add(TIMEOUT, lambda: win.destroy())
@@ -33,6 +25,6 @@ class TestRecommendationsWidgets(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    import logging
+    #import logging
     #logging.basicConfig(level=logging.DEBUG)
     unittest.main()
