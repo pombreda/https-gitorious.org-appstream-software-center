f2b40337f10b1f129038162bf18011b53c8eed69
Gary Lasker <gary.lasker@canonical.com>
2012-03-06 15:48:26 -0500

    RED: improve failure test case, still failing and new ready for fix

--- test/gtk3/test_unity_launcher_integration.py
+++ test/gtk3/test_unity_launcher_integration.py
@@ -16,13 +16,109 @@ from softwarecenter.db.application import Application
 from softwarecenter.ui.gtk3.panes.availablepane import get_test_window
 from softwarecenter.backend.unitylauncher import UnityLauncherInfo
 
+# Tests for Ubuntu Software Center's integration with the Unity launcher,
+# see https://wiki.ubuntu.com/SoftwareCenter#Learning%20how%20to%20launch%20an%20application
+
 # we can only have one instance of availablepane, so create it here
 win = get_test_window()
 available_pane = win.get_data("pane")
 
-# see https://wiki.ubuntu.com/SoftwareCenter#Learning%20how%20to%20launch%20an%20application
-
 class TestUnityLauncherIntegration(unittest.TestCase):
+
+    def setUp(self):
+        # monkey patch is_unity_running
+        softwarecenter.utils.is_unity_running = lambda: True
+        
+    def test_unity_launcher_integration_list_view(self):
+        # test the automatic add to launcher enabled functionality when
+        # installing an app form the list view
+        available_pane.add_to_launcher_enabled = True
+        test_pkgname = "lincity-ng"
+        # now pretend
+        # for testing, we substitute a fake version of UnityLauncher's
+        # send_application_to_launcher method that lets us check for the
+        # correct values and also avoids firing the actual dbus signal
+        # to the unity launcher service
+        self.expected_pkgname = test_pkgname
+        self.expected_launcher_info = UnityLauncherInfo("lincity-ng",
+                 "lincity-ng",
+                 0, 0, 0, 0, # these values are set in availablepane
+                 "/usr/share/app-install/desktop/lincity-ng:lincity-ng.desktop",
+                 "testid101")
+        available_pane.unity_launcher.send_application_to_launcher = (
+                self._fake_send_application_to_launcher_and_check)
+        self._install_from_list_view(test_pkgname)
+
+    def test_unity_launcher_integration_details_view(self):
+        # test the automatic add to launcher enabled functionality when
+        # installing an app from the details view
+        available_pane.add_to_launcher_enabled = True
+        test_pkgname = "lincity-ng"
+        # now pretend
+        # for testing, we substitute a fake version of UnityLauncher's
+        # send_application_to_launcher method that lets us check for the
+        # correct values and also avoids firing the actual dbus signal
+        # to the unity launcher service
+        self.expected_pkgname = test_pkgname
+        self.expected_launcher_info = UnityLauncherInfo("lincity-ng",
+                 "lincity-ng",
+                 0, 0, 0, 0, # these values are set in availablepane
+                 "/usr/share/app-install/desktop/lincity-ng:lincity-ng.desktop",
+                 "testid101")
+        available_pane.unity_launcher.send_application_to_launcher = (
+                self._fake_send_application_to_launcher_and_check)
+        self._navigate_to_appdetails_and_install(test_pkgname)
+        
+    def test_unity_launcher_integration_disabled(self):
+        # test the case where automatic add to launcher is disabled
+        available_pane.add_to_launcher_enabled = False
+        test_pkgname = "lincity-ng"
+        # now pretend
+        # for testing, we substitute a fake version of UnityLauncher's
+        # send_application_to_launcher method that lets us check for the
+        # correct values and also avoids firing the actual dbus signal
+        # to the unity launcher service
+        # in the disabled add to launcher case, we just want to insure
+        # that we never call send_application_to_launcher, so we can just
+        # plug in bogus values and we will catch a call if it occurs
+        self.expected_pkgname = ""
+        self.expected_launcher_info = UnityLauncherInfo("", "",
+                 0, 0, 0, 0, "", "")
+        available_pane.unity_launcher.send_application_to_launcher = (
+                self._fake_send_application_to_launcher_and_check)
+        self._navigate_to_appdetails_and_install(test_pkgname)
+
+    def test_desktop_file_path_conversion(self):
+        # test 'normal' case
+        app_install_desktop_path = ("./data/app-install/desktop/" +
+                                    "deja-dup:deja-dup.desktop")
+        installed_desktop_path = convert_desktop_file_to_installed_location(
+                app_install_desktop_path, "deja-dup")
+        self.assertEqual(installed_desktop_path,
+                         "./data/applications/deja-dup.desktop")
+        # test encoded subdirectory case, e.g. e.g. kde4_soundkonverter.desktop
+        app_install_desktop_path = ("./data/app-install/desktop/" +
+                                    "soundkonverter:" +
+                                    "kde4__soundkonverter.desktop")
+        installed_desktop_path = convert_desktop_file_to_installed_location(
+                app_install_desktop_path, "soundkonverter")
+        self.assertEqual(installed_desktop_path,
+                         "./data/applications/kde4/soundkonverter.desktop")
+        # test the for-purchase case (uses "software-center-agent" as its
+        # appdetails.desktop_file value)
+        # FIXME: this will only work if update-manager is installed
+        app_install_desktop_path = "software-center-agent"
+        installed_desktop_path = convert_desktop_file_to_installed_location(
+                app_install_desktop_path, "update-manager")
+        self.assertEqual(installed_desktop_path,
+                         "/usr/share/applications/update-manager.desktop")
+        # test case where we don't have a value for app_install_desktop_path
+        # (e.g. for a local .deb install, see bug LP: #768158)
+        installed_desktop_path = convert_desktop_file_to_installed_location(
+                None, "update-manager")
+        # FIXME: this will only work if update-manager is installed
+        self.assertEqual(installed_desktop_path,
+                         "/usr/share/applications/update-manager.desktop")
     
     def _zzz(self):
         for i in range(10):
@@ -33,11 +129,9 @@ class TestUnityLauncherIntegration(unittest.TestCase):
         while Gtk.events_pending():
             Gtk.main_iteration()
 
-    def setUp(self):
-        # monkey patch is_unity_running
-        softwarecenter.utils.is_unity_running = lambda: True
-        
     def _install_from_list_view(self, pkgname):
+        from softwarecenter.ui.gtk3.panes.availablepane import AvailablePane
+        available_pane.notebook.set_current_page(AvailablePane.Pages.LIST)
         app = Application("", pkgname)
         available_pane.app_view.emit("application-selected",
                                      app)
@@ -78,98 +172,7 @@ class TestUnityLauncherIntegration(unittest.TestCase):
                 self.expected_launcher_info.app_install_desktop_file_path)
         self.assertEqual(launcher_info.trans_id,
                 self.expected_launcher_info.trans_id)
-                
-    def test_unity_launcher_integration_list_view(self):
-        # test the automatic add to launcher enabled functionality when
-        # installing an app form the list view
-        available_pane.add_to_launcher_enabled = True
-        test_pkgname = "lincity-ng"
-        # now pretend
-        # for testing, we substitute a fake version of UnityLauncher's
-        # send_application_to_launcher method that lets us check for the
-        # correct values and also avoids firing the actual dbus signal
-        # to the unity launcher service
-        self.expected_pkgname = test_pkgname
-        self.expected_launcher_info = UnityLauncherInfo("lincity-ng",
-                 "lincity-ng",
-                 0, 0, 0, 0, # these values are set in availablepane
-                 "/usr/share/app-install/desktop/lincity-ng:lincity-ng.desktop",
-                 "testid101")
-        available_pane.unity_launcher.send_application_to_launcher = (
-                self._fake_send_application_to_launcher_and_check)
-        self._install_from_list_view(test_pkgname)
 
-#    def test_unity_launcher_integration_details_view(self):
-#        # test the automatic add to launcher enabled functionality when
-#        # installing an app from the details view
-#        available_pane.add_to_launcher_enabled = True
-#        test_pkgname = "lincity-ng"
-#        # now pretend
-#        # for testing, we substitute a fake version of UnityLauncher's
-#        # send_application_to_launcher method that lets us check for the
-#        # correct values and also avoids firing the actual dbus signal
-#        # to the unity launcher service
-#        self.expected_pkgname = test_pkgname
-#        self.expected_launcher_info = UnityLauncherInfo("lincity-ng",
-#                 "lincity-ng",
-#                 0, 0, 0, 0, # these values are set in availablepane
-#                 "/usr/share/app-install/desktop/lincity-ng:lincity-ng.desktop",
-#                 "testid101")
-#        available_pane.unity_launcher.send_application_to_launcher = (
-#                self._fake_send_application_to_launcher_and_check)
-#        self._navigate_to_appdetails_and_install(test_pkgname)
-#        
-#    def test_unity_launcher_integration_disabled(self):
-#        # test the case where automatic add to launcher is disabled
-#        available_pane.add_to_launcher_enabled = False
-#        test_pkgname = "lincity-ng"
-#        # now pretend
-#        # for testing, we substitute a fake version of UnityLauncher's
-#        # send_application_to_launcher method that lets us check for the
-#        # correct values and also avoids firing the actual dbus signal
-#        # to the unity launcher service
-#        # in the disabled add to launcher case, we just want to insure
-#        # that we never call send_application_to_launcher, so we can just
-#        # plug in bogus values and we will catch a call if it occurs
-#        self.expected_pkgname = ""
-#        self.expected_launcher_info = UnityLauncherInfo("", "",
-#                 0, 0, 0, 0, "", "")
-#        available_pane.unity_launcher.send_application_to_launcher = (
-#                self._fake_send_application_to_launcher_and_check)
-#        self._navigate_to_appdetails_and_install(test_pkgname)
-
-    def test_desktop_file_path_conversion(self):
-        # test 'normal' case
-        app_install_desktop_path = ("./data/app-install/desktop/" +
-                                    "deja-dup:deja-dup.desktop")
-        installed_desktop_path = convert_desktop_file_to_installed_location(
-                app_install_desktop_path, "deja-dup")
-        self.assertEqual(installed_desktop_path,
-                         "./data/applications/deja-dup.desktop")
-        # test encoded subdirectory case, e.g. e.g. kde4_soundkonverter.desktop
-        app_install_desktop_path = ("./data/app-install/desktop/" +
-                                    "soundkonverter:" +
-                                    "kde4__soundkonverter.desktop")
-        installed_desktop_path = convert_desktop_file_to_installed_location(
-                app_install_desktop_path, "soundkonverter")
-        self.assertEqual(installed_desktop_path,
-                         "./data/applications/kde4/soundkonverter.desktop")
-        # test the for-purchase case (uses "software-center-agent" as its
-        # appdetails.desktop_file value)
-        # FIXME: this will only work if update-manager is installed
-        app_install_desktop_path = "software-center-agent"
-        installed_desktop_path = convert_desktop_file_to_installed_location(
-                app_install_desktop_path, "update-manager")
-        self.assertEqual(installed_desktop_path,
-                         "/usr/share/applications/update-manager.desktop")
-        # test case where we don't have a value for app_install_desktop_path
-        # (e.g. for a local .deb install, see bug LP: #768158)
-        installed_desktop_path = convert_desktop_file_to_installed_location(
-                None, "update-manager")
-        # FIXME: this will only work if update-manager is installed
-        self.assertEqual(installed_desktop_path,
-                         "/usr/share/applications/update-manager.desktop")
-        
 
 if __name__ == "__main__":
     unittest.main()
