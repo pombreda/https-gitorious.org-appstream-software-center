b3dac9050dca145a822a603a220a2d61fbfdd5bb
Gary Lasker <gary.lasker@canonical.com>
2012-03-07 17:27:15 -0500

    enable the published_time unit test, this was disabled pending deployment of the feature on the staging server, which happened a while ago now, also remove a unit test that no longer applies (and that was also disabled anyway)

--- test/test_database.py
+++ test/test_database.py
@@ -319,9 +319,7 @@ class TestDatabase(unittest.TestCase):
             doc.get_value(value_time) >= last_time
             last_time = doc.get_value(value_time)
 
-    # FIXME: re-enable both tests once sc.staging.ubuntu.com has the new
-    #        version of scagent with support for the publication date
-    def disabled_test_for_purchase_apps_date_published(self):
+    def test_for_purchase_apps_date_published(self):
         from softwarecenter.testutils import get_test_pkg_info
         #os.environ["SOFTWARE_CENTER_DEBUG_HTTP"] = "1"
         os.environ["SOFTWARE_CENTER_AGENT_HOST"] = "http://sc.staging.ubuntu.com/"
@@ -339,36 +337,6 @@ class TestDatabase(unittest.TestCase):
             # make sure that a date_published value is provided
             self.assertNotEqual(date_published, "")
             self.assertNotEqual(date_published, None)
-        del os.environ["SOFTWARE_CENTER_AGENT_HOST"]
-
-    def disabled_test_for_purchase_apps_cataloged_time(self):
-        from softwarecenter.testutils import get_test_pkg_info
-        #os.environ["SOFTWARE_CENTER_DEBUG_HTTP"] = "1"
-        os.environ["SOFTWARE_CENTER_AGENT_HOST"] = "http://sc.staging.ubuntu.com/"
-        # staging does not have a valid cert
-        os.environ["PISTON_MINI_CLIENT_DISABLE_SSL_VALIDATION"] = "1"
-        cache = get_test_pkg_info()
-        db = xapian.WritableDatabase("./data/test.db",
-                                     xapian.DB_CREATE_OR_OVERWRITE)
-        res = update_from_software_center_agent(db, cache, ignore_cache=True)
-        self.assertTrue(res)
-        res = update_from_app_install_data(db, self.cache, datadir="./data/desktop")
-        self.assertTrue(res)
-        db = StoreDatabase("./data/test.db", self.cache)
-        db.open(use_axi=True)
-
-        axi_value_time = db._axi_values["catalogedtime"]
-        sc_app = Application("Ubuntu Software Center Test", "software-center")
-        sc_doc = db.get_xapian_document(sc_app.appname, sc_app.pkgname)
-        sc_cataloged_time = sc_doc.get_value(axi_value_time)
-        for_purch_app = Application("For Purchase Test App", "hellox")
-        for_purch_doc = db.get_xapian_document(for_purch_app.appname,
-                                               for_purch_app.pkgname)
-        for_purch_cataloged_time = for_purch_doc.get_value(axi_value_time)
-        # the for-purchase test package should be cataloged at a
-        # later time than axi package Ubuntu Software Center
-        self.assertTrue(for_purch_cataloged_time > sc_cataloged_time)
-
         del os.environ["SOFTWARE_CENTER_AGENT_HOST"]
 
     def test_hardware_requirements_satisfied(self):
