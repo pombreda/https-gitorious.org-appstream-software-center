818e9cabc4a1c71cd8819a9e4f462ff5d54a5f01
Anthony Lenton <anthony.lenton@canonical.com>
2012-03-13 15:48:10 -0300

    Put back run_against_rnr_testing_env.sh.

--- run_against_rnr_testing_env.sh
+++ run_against_rnr_testing_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export SOFTWARE_CENTER_REVIEWS_HOST="http://184.82.116.62/reviews/api/1.0"
-export SOFTWARE_CENTER_RECOMMENDER_HOST="http://rec.staging.ubuntu.com"
+
 
 # sso
 export USSOC_SERVICE_URL="https://login.staging.ubuntu.com/api/1.0"
