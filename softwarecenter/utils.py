1f6a0144aac0da29dceab6555e113aeeb551537b
Vincent Untz <vuntz@opensuse.org>
2012-03-15 16:55:13 +0100

    Make a log less scary when running from checkout

--- softwarecenter/utils.py
+++ softwarecenter/utils.py
@@ -625,8 +625,8 @@ def mangle_paths_if_running_in_local_checkout():
             softwarecenter.paths.APP_INSTALL_PATH
         if not os.path.exists(path):
             softwarecenter.paths.APP_INSTALL_PATH = './build/share/app-install'
-            logging.warn("using local APP_INSTALL_PATH: %s" %\
-                             softwarecenter.paths.APP_INSTALL_PATH)
+            logging.getLogger("softwarecenter").warn("using local APP_INSTALL_PATH: %s" %\
+                                                         softwarecenter.paths.APP_INSTALL_PATH)
     else:
         datadir = softwarecenter.paths.datadir
         xapian_base_path = softwarecenter.paths.XAPIAN_BASE_PATH
