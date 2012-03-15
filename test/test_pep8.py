b5de8b45bb408e9ea2e437059214b6cc6d5937e0
Anthony Lenton <anthony.lenton@canonical.com>
2012-03-14 23:43:26 -0300

    All files in softwarecenter.ui.gtk3 except one pass the pep8 test.

--- test/test_pep8.py
+++ test/test_pep8.py
@@ -8,25 +8,15 @@ setup_test_env()
 
 # Only test these two packages for now:
 import softwarecenter.db.pkginfo_impl
-import softwarecenter.ui.gtk3.dialogs
-import softwarecenter.ui.gtk3.models
-import softwarecenter.ui.gtk3.panes
-import softwarecenter.ui.gtk3.session
-import softwarecenter.ui.gtk3.views
-import softwarecenter.ui.gtk3.widgets
+import softwarecenter.ui.gtk3
 import softwarecenter.ui.qml
 
 class PackagePep8TestCase(unittest.TestCase):
     maxDiff = None
     packages = [softwarecenter.ui.qml,
-                softwarecenter.ui.gtk3.dialogs,
-                softwarecenter.ui.gtk3.models,
-                softwarecenter.ui.gtk3.panes,
-                softwarecenter.ui.gtk3.session,
-                softwarecenter.ui.gtk3.views,
-                softwarecenter.ui.gtk3.widgets,
+                softwarecenter.ui.gtk3,
                 softwarecenter.db.pkginfo_impl]
-    exclude = []
+    exclude = ['review_gui_helper.py']
 
     def message(self, text):
         self.errors.append(text)
