9e002a9deed4831bc68d6b241bcfdd4ccee3d6c1
Natalia B. Bidart <natalia.bidart@canonical.com>
2012-05-31 09:55:41 -0300

    Run-test script cleanup.

--- run-tests.sh
+++ run-tests.sh
@@ -58,7 +58,7 @@ if [ $COVERAGE ]; then
     # If you are measuring coverage in a multi-process program, or across a
     # number of machines, you’ll want the --parallel-mode switch to keep the
     # data separate during measurement. See Combining data files below.
-#    COVERAGE_CMDLINE="$COVERAGE run --parallel-mode"
+    ##COVERAGE_CMDLINE="$COVERAGE run --parallel-mode"
     echo "No coverage for now."
 fi
 
@@ -66,8 +66,6 @@ PYTHON="$XVFB_CMDLINE $COVERAGE_CMDLINE python -m unittest"
 
 # and record failures here
 OUTPUT=$TESTS_DIR"/output"
-rm -rf $OUTPUT
-mkdir $OUTPUT
 
 FAILED=""
 run_tests_for_dir() {
@@ -85,16 +83,15 @@ run_tests_for_dir() {
     done
 }
 
-
-# run the requested tests if arguments were given,
-# otherwise run the whole suite
-# example of custom params (discover all the tests under the tests/gtk3 dir):
-
-# ./run-tests.sh discover -v -s tests/gtk3/
-
-# See http://docs.python.org/library/unittest.html#test-discovery
-# for more info.
 if [ $# -gt 0 ]; then
+    # run the requested tests if arguments were given,
+    # otherwise run the whole suite
+    # example of custom params (discover all the tests under the tests/gtk3 dir):
+
+    # ./run-tests.sh discover -v -s tests/gtk3/
+
+    # See http://docs.python.org/library/unittest.html#test-discovery
+    # for more info.
     RUN_TESTS="$PYTHON $@"
     echo "Running the command: $RUN_TESTS"
     $RUN_TESTS
@@ -103,28 +100,19 @@ else
     # using discovery, but there is too much interference between tests in
     # order to do so, so we need a new python process per test file.
     ##RUN_TESTS="$PYTHON discover -v -c -b"
+    rm -rf $OUTPUT
+    mkdir $OUTPUT
     run_tests_for_dir $TESTS_DIR
     run_tests_for_dir "$TESTS_DIR/gtk3"
-    #for i in $(find $TESTS_DIR -maxdepth 1 -name 'test_*.py'); do
-    #    TEST_NAME=$(basename $i | cut -d '.' -f 1)
-    #    printf '%-50s' "Testing $TEST_NAME..."
-    #    if ! $PYTHON -v -c -b tests.$TEST_NAME > $OUTPUT/$TEST_NAME.out 2>&1; then
-    #        FAILED="$FAILED $TEST_NAME"
-    #        echo "[ FAIL ]"
-    #    else
-    #        echo "[  OK  ]"
-    #        rm -f ${OUTPUT}/$file.out;
-    #    fi
-    #done
-fi
 
-# gather the coverage data
-#./gen-coverage-report.sh
+    # gather the coverage data
+    ##./gen-coverage-report.sh
 
-if [ -n "$FAILED" ]; then
-    echo "FAILED: $FAILED"
-    echo "Check ${OUTPUT}/ directory for the details"
-    exit 1
-else
-    echo "All OK!"
+    if [ -n "$FAILED" ]; then
+        echo "FAILED: $FAILED"
+        echo "Check ${OUTPUT}/ directory for the details"
+        exit 1
+    else
+        echo "All OK!"
+    fi
 fi
